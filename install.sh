1f34f39590d9ba6206b2a64ceba72061d4a73670
decoder <decoder@live.de>
2021-09-04 16:30:11 +0200

    auto commit

--- install.sh
+++ install.sh
@@ -51,7 +51,7 @@ process "→ setup git config"
 
 process "→ install essencial packages"
 
-  sudo apt install -y vim-gtk htop unzip python3-setuptools figlet tmux pydf mc wget mtr ncdu cmatrix ranger jq lolcat
+  sudo apt install -y vim-gtk htop unzip python3-setuptools figlet tmux pydf mc wget mtr ncdu cmatrix ranger jq lolcat tmux
 
 process "→ install pip"
 
