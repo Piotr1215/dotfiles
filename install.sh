df4ec998f4ae76a44fd5698c0f1abb99b09e7fa9
decoder <decoder@live.de>
2021-09-04 13:03:55 +0200

    auto commit

--- install.sh
+++ install.sh
@@ -47,36 +47,36 @@ process "→ install essencial packages"
 
 sudo apt install -y vim-gtk htop unzip python3-setuptools figlet tmux pydf mc wget mtr ncdu cmatrix ranger jq gdu lsd lolcat
 
-echo '[STEP 3] install pip'
+process "→ install pip"
 
 sudo apt install -y python3-pip
 
-echo '[STEP 4] install go'
+process "→ install go"
 
 sudo apt install -y golang
 
-echo '[STEP 5] install kubectl'
+process "→ install kubectl"
 
 cd /usr/local/bin
 sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 
-echo '[STEP 6] install helm'
+process "→ install helm"
 
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
-echo '[STEP 7] install terraform'
+process "→ install terraform"
 
 cd /tmp
 wget https://releases.hashicorp.com/terraform/0.13.4/terraform_0.13.4_linux_amd64.zip
 unzip terraform_0.13.4_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
 
-echo '[STEP 8] install node and nmp'
+process "→ install node and nmp"
 
 sudo apt install -y nodejs npm
 
-echo '[STEP 9] install zsh'
+process "→ install zsh"
 
 sudo rm -rf ~/.oh-my-zsh
 
