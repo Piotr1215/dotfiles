106e300205d206a2c9bd1170e8238a44ac4709d3
decoder <decoder@live.de>
2021-11-19 23:01:00 +0100

    auto commit

--- install.sh
+++ install.sh
@@ -1,6 +1,40 @@
 #!/usr/bin/env bash
 
-set -e
+set -euo pipefail
+IFS=$'\n\t'
+
+# Define a function which rename a `target` file to `target.backup` if the file
+# exists and if it's a 'real' file, ie not a symlink
+backup() {
+  target=$1
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      mv "$target" "$target.backup"
+      echo "-----> Moved your old $target config file to $target.backup"
+    fi
+  fi
+}
+
+symlink() {
+  file=$1
+  link=$2
+  if [ ! -e "$link" ]; then
+    echo "-----> Symlinking your new $link"
+    ln -s $file $link
+  fi
+}
+
+# For all files `$name` in the present folder except `*.sh`, `README.md`, `settings.json`,
+# and `config`, backup the target file located at `~/.$name` and symlink `$name` to `~/.$name`
+for name in *; do
+  if [ ! -d "$name" ]; then
+    target="$HOME/.$name"
+    if [[ ! "$name" =~ '\.sh$' ]] && [ "$name" != 'README.md' ] && [[ "$name" != 'settings.json' ]] && [[ "$name" != 'config' ]]; then
+      backup $target
+      symlink $PWD/$name $target
+    fi
+  fi
+done
 
 # Set variables
 NAME=$1
