0dd9f0e752c1befa8ec9fb722736415b0f196fc3
decoder <decoder@live.de>
2022-02-12 20:24:42 +0100

    auto commit

--- install.sh
+++ install.sh
@@ -90,6 +90,7 @@ process "→ install essencial packages"
 
   sudo apt install -y vim-gtk htop unzip python3-setuptools figlet tmux pydf mc wget mtr ncdu cmatrix ranger jq lolcat tmux exa
   ln -sf ${HOME}/dotfiles/.tmux.conf ~/.tmux.conf
+
 process "→ install pip"
 
   sudo apt install -y python3-pip
