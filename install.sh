c9a8c9df8f45c6d653ebb8f0f091cfa7e154fbaf
decoder <decoder@live.de>
2022-02-12 21:31:20 +0100

    auto commit

--- install.sh
+++ install.sh
@@ -143,7 +143,7 @@ process "→ Installing zsh-autosuggestions plugin"
 
   git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # process "→ Installing stern"
 # (
