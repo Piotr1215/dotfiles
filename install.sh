d8e58f065c9152a32caa501dfa26910b52c03353
decoder <decoder@live.de>
2022-02-12 17:48:41 +0100

    auto commit

--- install.sh
+++ install.sh
@@ -160,7 +160,7 @@ process "→ Installing Arkade"
   curl -sLS https://get.arkade.dev | sudo sh
 
 process "→ Setting zsh as default shell"
-  sudo chsh -s $(which zsh) root
+sudo chsh -s $(which zsh) $(whoami)
   zsh
   sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="3den"/g' ~/.zshrc
 
