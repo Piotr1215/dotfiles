75af5c62b79a30d630701dd062bef18ee602ce0a
decoder <decoder@live.de>
2022-02-12 23:00:05 +0100

    auto commit

--- install.sh
+++ install.sh
@@ -89,7 +89,7 @@ process "→ setup git config"
 
 process "→ install essencial packages"
 
-  sudo apt install -y vim-gtk htop unzip python3-setuptools figlet tmux pydf mc wget mtr ncdu cmatrix ranger jq lolcat tmux bat
+  sudo apt install -y vim-gtk htop unzip python3-setuptools figlet tmux pydf mc wget mtr ncdu cmatrix ranger jq lolcat tmux bat locate
   ln -sf ${HOME}/dotfiles/.tmux.conf ~/.tmux.conf
 
 process "→ install pip"
