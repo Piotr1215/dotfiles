9bfc01f6183965f974e52801b9279a2851ef1bbb
Piotr Zaniewski <piotr@upbound.io>
2022-05-26 21:18:48 +0200

    auto commit

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,4 +1,4 @@
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = '~/.config/nvim/data/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({
