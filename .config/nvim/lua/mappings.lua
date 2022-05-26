bc6ae72e0fe855cacdf08ddeb4595e627e84924a
Piotr Zaniewski <piotr@upbound.io>
2022-05-26 17:58:47 +0200

    auto commit

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,31 @@
+vim.g.mapLeader = ' '
+
+function map(mode, shortcut, command)
+  vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
+end
+
+function nmap(shortcut, command)
+  map('n', shortcut, command)
+end
+
+function imap(shortcut, command)
+  map('i', shortcut, command)
+end
+
+function vmap(shortcut, command)
+  map('v', shortcut, command)
+end
+
+function cmap(shortcut, command)
+  map('c', shortcut, command)
+end
+
+function tmap(shortcut, command)
+  map('t', shortcut, command)
+end
+
+function xmap(shortcut, command)
+  map('x', shortcut, command)
+end
+
+
