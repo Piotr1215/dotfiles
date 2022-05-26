ddcf05def38aa9ddcd95689f16b19b258330b928
Piotr Zaniewski <piotr@upbound.io>
2022-05-26 21:43:14 +0200

    auto commit

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,6 +4,10 @@ function map(mode, shortcut, command)
   vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
 end
 
+function emap(shortcut, command)
+  map('', shortcut, command)
+end
+
 function nmap(shortcut, command)
   map('n', shortcut, command)
 end
@@ -35,4 +39,11 @@ nmap("<leader>k", ":m .-2<CR>==")            -- Move Line Up in Normal Mode
 nmap("<leader>j", ":m .+1<CR>==")            -- Move Line Down in Normal Mode
 
 nmap("<Leader>nh", ":.,/^#/<CR>")            -- Got to next markdown header
+
+-- SEARCH & REPLACE --
+-- Easy Motion Mappings
+emap("<Leader>o", "<Plug>(easymotion-prefix")
+emap("<Leader>of", "<Plug>(easymotion-bd-f")
+emap("<Leader>ol", "<Plug>(easymotion-bd-w")
+nmap("<Leader>oc", "<Plug>(easymotion-overwin-f2")
 
