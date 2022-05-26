4ae0036201e02b04317f938d277a638a9e0ba259
Piotr Zaniewski <piotr@upbound.io>
2022-05-27 01:18:06 +0200

    auto commit

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -17,6 +17,31 @@ return require('packer').startup(function()
   use 'kdheepak/lazygit.nvim'
   use 'karoliskoncevicius/vim-sendtowindow'
   use 'jpalardy/vim-slime'
-  use { 'ldelossa/gh.nvim' requires = { { 'ldelossa/litee.nvim' } } }
+-- Editing Related
+  use 'Raimondi/delimitMate'
+  use 'godlygeek/tabular'
+  use 'plasticboy/vim-markdown'
+  use 'mattn/webapi-vim'
+  use 'Yggdroot/indentLine'
+  use 'tpope/vim-surround'
+  use 'mattn/emmet-vim'
+  use 'wellle/targets.vim'
+  use 'vim-syntastic/syntastic'
+  use 'sheerun/vim-polyglot'
+  use 'iamcco/markdown-preview.nvim'
+  use 'junegunn/vim-emoji'
+  use 'christoomey/vim-system-copy'
+  use 'dhruvasagar/vim-table-mode'
+  use 'junegunn/limelight.vim'
+  use 'ferrine/md-img-paste.vim'
+  use 'SidOfc/mkdx'
+  use 'weirongxu/plantuml-previewer.vim'
+  use 'tyru/open-browser.vim'
+  use 'dhruvasagar/vim-open-url'
+  use 'hrsh7th/vim-vsnip'
+  use 'hrsh7th/vim-vsnip-integ'
+  use 'sakshamgupta05/vim-todo-highlight'
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use 'preservim/nerdcommenter'
 end)
 
