4ca9f35a28e9467daa74e47399cca6d3d28cb077
Piotr Zaniewski <piotr@upbound.io>
2022-05-26 21:15:55 +0200

    auto commit

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,3 +1,15 @@
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({
+    'git',
+    'clone',
+    '--depth', '1',
+    'https://github.com/wbthomason/packer.nvim',
+    install_path
+  })
+end
+
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
 end)
