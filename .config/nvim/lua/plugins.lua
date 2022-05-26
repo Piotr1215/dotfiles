5baf5496cf6316043394931214688a804b053256
Piotr Zaniewski <piotr@upbound.io>
2022-05-27 01:14:24 +0200

    auto commit

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,5 +1,6 @@
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
+-- Editor Functionality
   use 'easymotion/vim-easymotion'
   use 'mhinz/vim-startify'
   use 'vim-airline/vim-airline'
@@ -11,4 +12,11 @@ return require('packer').startup(function()
   use 'nvim-telescope/telescope.nvim'
   use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
   use 'voldikss/vim-floaterm'
+  use 'rafamadriz/friendly-snippets'
+  use 'cljoly/telescope-repo.nvim'
+  use 'kdheepak/lazygit.nvim'
+  use 'karoliskoncevicius/vim-sendtowindow'
+  use 'jpalardy/vim-slime'
+  use { 'ldelossa/gh.nvim' requires = { { 'ldelossa/litee.nvim' } } }
 end)
+
