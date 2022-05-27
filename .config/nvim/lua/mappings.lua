ba51a93dc411982051b6dc45ad830f769137aeb4
Piotr Zaniewski <piotr@upbound.io>
2022-05-28 01:44:44 +0200

    auto commit

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -146,21 +146,21 @@ nmap('<leader>gg', ':LazyGit<CR>')
 
 -- NAVIGATION --
 -- Netrw settings
-nmap('<leader>dd', ':Lexplore %:p:h<CR>')
-nmap('<Leader>da', ':Lexplore<CR>')
--- Save buffer
-nmap('<leader>w', ':w<CR>')
--- Move screen to contain current line at the top
-nmap('<leader>sv', ':source ${HOME}/.config/nvim/init.vim<CR>')
--- jj in insert mode instead of ESC
-imap('jj', '<Esc>')
-imap('jk', '<Esc>')
--- Zoom split windows
---nmap('Zz', '<c-w>_ \| <c-w>\|')
---nmap('Zo', '<c-w>=')
--- Split navigation
-nmap('<S-L>', '<C-W><C-L>')
-nmap('<S-H>', '<C-W><C-H>')
-nmap('<S-U>', '<C-W><C-K>')
-nmap('<S-J>', '<C-W><C-J>')
+--nmap('<leader>dd', ':Lexplore %:p:h<CR>')
+--nmap('<Leader>da', ':Lexplore<CR>')
+---- Save buffer
+--nmap('<leader>w', ':w<CR>')
+---- Move screen to contain current line at the top
+--nmap('<leader>sv', ':source ${HOME}/.config/nvim/init.vim<CR>')
+---- jj in insert mode instead of ESC
+--imap('jj', '<Esc>')
+--imap('jk', '<Esc>')
+---- Zoom split windows
+----nmap('Zz', '<c-w>_ \| <c-w>\|')
+----nmap('Zo', '<c-w>=')
+---- Split navigation
+--nmap('<S-L>', '<C-W><C-L>')
+--nmap('<S-H>', '<C-W><C-H>')
+--nmap('<S-U>', '<C-W><C-K>')
+--nmap('<S-J>', '<C-W><C-J>')
 
