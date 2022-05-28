260d4e5f690abf51af4f42b49fb9085a4dd6586c
Piotr Zaniewski <piotr@upbound.io>
2022-05-28 19:37:12 +0200

    auto commit

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -57,20 +57,6 @@ return require('packer').startup(function()
     "williamboman/nvim-lsp-installer",
     {
         "neovim/nvim-lspconfig",
-        config = function()
-          require("nvim-lsp-installer").setup({
-              automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
-              ui = {
-                  icons = {
-                      server_installed = "✓",
-                      server_pending = "➜",
-                      server_uninstalled = "✗"
-                  }
-              }
-          })
-          local lspconfig = require("lspconfig")
-          lspconfig.sumneko_lua.setup {}
-        end
     }
   }
   -- Show VSCode-esque pictograms
