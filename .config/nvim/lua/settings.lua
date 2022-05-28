24281ac3791be9dbdb8e5e391a25f8e0b597f488
Piotr Zaniewski <piotr@upbound.io>
2022-05-28 19:45:40 +0200

    auto commit

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -11,19 +11,19 @@ vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true,
 vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
 
 set.background = 'dark'
-set.ignorecase = true             -- ignore case in search
-set.smartcase = true              -- do not ignore case with capitals
+set.ignorecase = true -- ignore case in search
+set.smartcase = true -- do not ignore case with capitals
 set.scrolloff = 8
 set.hlsearch = true
 set.updatetime = 300
 set.autochdir = true
 
-set.splitright = true             -- put new windows below current
+set.splitright = true -- put new windows below current
 
 set.mouse = v
 set.number = true
 set.encoding = "utf-8"
-cmd('set.backspace = indent,eol,start')
+cmd('set backspace=indent,eol,start')
 set.cursorline = true
 
 set.expandtab = true
@@ -38,3 +38,6 @@ cmd('colorscheme PaperColor')
 
 -- Plugins Settings --
 vim.g.limelight_conceal_ctermfg = 'gray'
+vim.g.limelight_conceal_ctermfg = 'gray'
+vim.g.limelight_conceal_ctermfg = 240
+vim.g.sendtowindow_use_defaults = 0
