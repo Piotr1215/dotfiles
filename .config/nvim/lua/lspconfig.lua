748d3d3e733dc515f81c112480634b363ac5b575
Piotr Zaniewski <piotr@upbound.io>
2022-05-28 14:23:29 +0200

    auto commit

--- .config/nvim/lua/lspconfig.lua
+++ .config/nvim/lua/lspconfig.lua
@@ -47,11 +47,8 @@ local servers = {
 }
 
 for _, lsp in pairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    flags = {
-      -- This will be the default in neovim 0.7+
-      debounce_text_changes = 150,
+    lsp[server].setup {
+        capabilities = capabilities,
+        on_attach = on_attach,
     }
-  }
 end
