53acf22b43ca2dd0086043eb9383118f72cd09ec
Piotr Zaniewski <piotr@upbound.io>
2022-05-28 14:13:44 +0200

    auto commit

--- .config/nvim/lua/lspconfig.lua
+++ .config/nvim/lua/lspconfig.lua
@@ -18,109 +18,6 @@ local servers = {
   "yamlls",
   "emmet_ls",
 }
-
-for _, name in pairs(servers) do
-  local server_is_found, server = lsp_installer.get_server(name)
-  if server_is_found and not server:is_installed() then
-    print("Installing " .. name)
-    server:install()
-  end
-end
-
-local on_attach = function(_, bufnr)
-  -- Create some shortcut functions.
-  -- NOTE: The `vim` variable is supplied by Neovim.
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  local function buf_set_option(...)
-    vim.api.nvim_buf_set_option(bufnr, ...)
-  end
-
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  local opts = {noremap=true, silent=true}
-
-  -- ======================= The Keymaps =========================
-  -- jump to definition
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-
-  -- Format buffer
-  buf_set_keymap('n', '<F3>', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-
-  -- Jump LSP diagnostics
-  -- NOTE: Currently, there is a bug in lspsaga.diagnostic module. Thus we use
-  --       Vim commands to move through diagnostics.
-  buf_set_keymap('n', '[g', ':Lspsaga diagnostic_jump_prev<CR>', opts)
-  buf_set_keymap('n', ']g', ':Lspsaga diagnostic_jump_next<CR>', opts)
-
-  -- Rename symbol
-  buf_set_keymap('n', '<leader>rn', "<cmd>lua require('lspsaga.rename').rename()<CR>", opts)
-
-  -- Find references
-  buf_set_keymap('n', 'gr', '<cmd>lua require("lspsaga.provider").lsp_finder()<CR>', opts)
-
-  -- Doc popup scrolling
-  buf_set_keymap('n', 'K', "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", opts)
-  buf_set_keymap('n', '<C-f>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", opts)
-  buf_set_keymap('n', '<C-b>', "<cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", opts)
-
-  -- codeaction
-  buf_set_keymap('n', '<leader>ac', "<cmd>lua require('lspsaga.codeaction').code_action()<CR>", opts)
-  buf_set_keymap('v', '<leader>a', ":<C-U>lua require('lspsaga.codeaction').range_code_action()<CR>", opts)
-
-  -- Floating terminal
-  -- NOTE: Use `vim.cmd` since `buf_set_keymap` is not working with `tnoremap...`
-  vim.cmd [[
-  nnoremap <silent> <A-d> <cmd>lua require('lspsaga.floaterm').open_float_terminal()<CR>
-  tnoremap <silent> <A-d> <C-\><C-n>:lua require('lspsaga.floaterm').close_float_terminal()<CR>
-  ]]
-end
-
-local server_specific_opts = {
-  sumneko_lua = function(opts)
-    opts.settings = {
-      Lua = {
-        -- NOTE: This is required for expansion of lua function signatures!
-        completion = {callSnippet = "Replace"},
-        diagnostics = {
-          globals = {'vim'},
-        },
-      },
-    }
-  end,
-
-  html = function(opts)
-    opts.filetypes = {"html", "htmldjango"}
-  end,
-}
-
--- `nvim-cmp` comes with additional capabilities, alongside the ones
--- provided by Neovim!
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
-lsp_installer.on_server_ready(function(server)
-  -- the keymaps, flags and capabilities that will be sent to the server as
-  -- options.
-  local opts = {
-    on_attach = on_attach,
-    flags = {debounce_text_changes = 150},
-    capabilities = capabilities,
-  }
-
-  -- If the current surver's name matches with the ones specified in the
-  -- `server_specific_opts`, set the options.
-  if server_specific_opts[server.name] then
-    server_specific_opts[server.name](opts)
-  end
-
-  -- And set up the server with our configuration!
-  server:setup(opts)
-end)
-
--- nvim-cmp
 local lspkind = require "lspkind"
 local cmp = require "cmp"
 
@@ -141,33 +38,36 @@ local feedkey = function(key, mode)
     )
 end
 
-local cmp_kinds = {
-  Text = "",
-  Method = "",
-  Function = "",
-  Constructor = "",
-  Field = "ﰠ",
-  Variable = "",
-  Class = "ﴯ",
-  Interface = "",
-  Module = "",
-  Property = "ﰠ",
-  Unit = "塞",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "פּ",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
+-- Fancy Autocompletion popup icons {{{3
+local CMP_KINDS = {
+    Class = "ﴯ",
+    Color = "",
+    Constant = "",
+    Constructor = "",
+    Enum = "",
+    EnumMember = "",
+    Event = "",
+    Field = "ﰠ",
+    File = "",
+    Folder = "",
+    Function = "",
+    Interface = "",
+    Keyword = "",
+    Method = "",
+    Module = "",
+    Operator = "",
+    Property = "ﰠ",
+    Reference = "",
+    Snippet = "",
+    Struct = "פּ",
+    Text = "",
+    TypeParameter = "",
+    Unit = "塞",
+    Value = "",
+    Variable = "",
 }
+-- 3}}}
+
 cmp.setup({
     formatting = {
         format = lspkind.cmp_format({
@@ -243,3 +143,224 @@ cmp.setup.cmdline(":", {
     }),
 })
 -- 2}}}
+local lsp_installer = require "nvim-lsp-installer"
+lsp_installer.setup({ ensure_installed = SERVERS })
+local lspconfig = require "lspconfig"
+
+-- Set up keymaps {{{3
+local on_attach = function(_, bufnr)
+    local opts = { noremap = true, silent = true, buffer = bufnr }
+
+    -- Lspsaga based config
+    vim.keymap.set("n", "<leader>rn", require("lspsaga.rename").rename, opts)
+    vim.keymap.set("n", "gr", require("lspsaga.provider").lsp_finder, opts)
+    vim.keymap.set("n", "]g", ":Lspsaga diagnostic_jump_next<CR>", opts)
+    vim.keymap.set("n", "[g", ":Lspsaga diagnostic_jump_prev<CR>", opts)
+    vim.keymap.set("n", "K", require("lspsaga.hover").render_hover_doc, opts)
+    vim.keymap.set("n", "<C-F>", function()
+        require("lspsaga.action").smart_scroll_with_saga(1)
+    end, opts)
+    vim.keymap.set("n", "<C-B>", function()
+        require("lspsaga.action").smart_scroll_with_saga(-1)
+    end, opts)
+    vim.keymap.set(
+        "n",
+        "gD",
+        require("lspsaga.provider").preview_definition,
+        opts
+    )
+    vim.keymap.set(
+        "n",
+        "<leader>ca",
+        require("lspsaga.codeaction").code_action,
+        opts
+    )
+    vim.keymap.set(
+        "v",
+        "<leader>ca",
+        ':<C-U>lua require("lspsaga.codeaction").range_code_action()<CR>',
+        opts
+    )
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set(
+        "n",
+        "<leader>wr",
+        vim.lsp.buf.remove_workspace_folder,
+        opts
+    )
+    vim.keymap.set("n", "<leader>wl", function()
+        vim.inspect(vim.lsp.buf.list_workspace_folders())
+    end, opts)
+    vim.api.nvim_create_user_command("Format", vim.lsp.buf.formatting, {})
+end
+-- 3}}}
+
+-- server-specific options {{{3
+local SERVER_SPECIFIC_OPTS = {
+    clangd = function(opts)
+        opts.cmd = {
+            "clangd",
+            "--clang-tidy",
+        }
+    end,
+    pylsp = function(opts)
+        opts.settings = {
+            pylsp = {
+                -- configurationSources = {"flake8"},
+                plugins = {
+                    jedi_completion = { include_params = true },
+                    flake8 = { enabled = true },
+                    pycodestyle = { enabled = false },
+                },
+            },
+        }
+    end,
+    sumneko_lua = function(opts)
+        opts.settings = {
+            Lua = {
+                -- NOTE: This is required for expansion of lua function signatures!
+                runtime = { version = "LuaJIT" },
+                workspace = {
+                    library = vim.api.nvim_get_runtime_file("", true),
+                },
+                completion = { callSnippet = "Replace" },
+                diagnostics = {
+                    globals = { "vim", "P" },
+                },
+            },
+        }
+    end,
+    html = function(opts)
+        opts.filetypes = { "html", "htmldjango" }
+    end,
+}
+-- 3}}}
+
+-- nvim-cmp supports additional completion capabilities
+local capabilities = require("cmp_nvim_lsp").update_capabilities(
+    vim.lsp.protocol.make_client_capabilities()
+)
+
+for _, server in ipairs(SERVERS) do
+    local opts = {
+        on_attach = on_attach,
+        -- flags = { debounce_text_changes = 150 },
+        capabilities = capabilities,
+    }
+
+    if SERVER_SPECIFIC_OPTS[server] then
+        SERVER_SPECIFIC_OPTS[server](opts)
+    end
+
+    lspconfig[server].setup(opts)
+end
+-- 2}}}
+
+-- lspsaga
+require("lspsaga").init_lsp_saga({
+    finder_action_keys = {
+        open = "<CR>",
+        quit = { "q", "<esc>" },
+    },
+    code_action_keys = {
+        quit = { "q", "<esc>" },
+    },
+    rename_action_keys = {
+        quit = "<esc>",
+    },
+})
+
+-- Lualine
+require("lualine").setup({
+    sections = {
+        lualine_c = {
+            { "filename", path = 1 },
+            "lsp_progress",
+        },
+    },
+})
+
+-- indent-blankline
+require("indent_blankline").setup({
+    -- for example, context is off by default, use this to turn it on
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+    filetype_exclude = { "help", "packer" },
+    buftype_exclude = { "terminal", "nofile" },
+    show_trailing_blankline_indent = false,
+})
+
+-- treesitter
+require("nvim-treesitter.configs").setup({
+    ensure_installed = {
+        "python",
+        "rust",
+        "c",
+        "cpp",
+        "bash",
+        "go",
+        "html",
+        "toml",
+    },
+    highlight = {
+        enable = true,
+    },
+})
+
+-- Tagbar
+vim.cmd [[
+nmap <F8> :TagbarOpenAutoClose<CR>
+]]
+
+-- FZF
+vim.cmd [[
+nnoremap <C-P> :Files<CR>
+nnoremap <Tab> :GFiles<CR>
+nnoremap <C-H> :GFiles?<CR>
+nnoremap <C-K> :BLines<CR>
+if executable('rg')
+    nnoremap <C-G> :Rg<CR>
+else
+    echom "Ripgrep (rg) is missing. Please install Ripgrep"
+endif
+]]
+
+-- auto-pairs-gentle
+vim.g.AutoPairs = {
+    ["("] = ")",
+    ["["] = "]",
+    ["{"] = "}",
+    ["'"] = "'",
+    ['"'] = '"',
+    ["`"] = "`",
+    -- ['<']='>',
+}
+
+-- vim-markdown
+vim.g.vim_markdown_folding_disabled = true
+
+-- vim-autoformat
+vim.cmd [[
+nnoremap <F3> :Autoformat<CR> :w<CR>
+]]
+
+-- markdown-preview
+vim.cmd [[
+nnoremap <M-m> :MarkdownPreviewToggle<CR>
+]]
+
+-- vim-python-pep8-indent
+vim.g.python_pep8_indent_multiline_string = -1
+
+-- vim-easy-align
+vim.cmd [[
+xmap ga <Plug>(EasyAlign)
+nmap ga <Plug>(EasyAlign)
+]]
+-- 1}}}
