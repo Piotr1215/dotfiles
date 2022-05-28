fdccf83e78a2b8e192abdc47376685c2494c3601
Piotr Zaniewski <piotr@upbound.io>
2022-05-28 14:06:11 +0200

    auto commit

--- .config/nvim/lua/lspconfig.lua
+++ .config/nvim/lua/lspconfig.lua
@@ -121,16 +121,24 @@ lsp_installer.on_server_ready(function(server)
 end)
 
 -- nvim-cmp
-local lspkind = require('lspkind')
-local cmp = require("cmp")
+local lspkind = require "lspkind"
+local cmp = require "cmp"
 
 local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0
+        and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+                :sub(col, col)
+                :match "%s"
+            == nil
 end
 
 local feedkey = function(key, mode)
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+    vim.api.nvim_feedkeys(
+        vim.api.nvim_replace_termcodes(key, true, true, true),
+        mode,
+        true
+    )
 end
 
 local cmp_kinds = {
