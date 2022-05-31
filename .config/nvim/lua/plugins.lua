d8b7b325576e5bfc14d6ff271bcd9a094537c6e6
Piotr Zaniewski <piotr@upbound.io>
2022-05-31 09:34:21 +0200

    Plugins sorted alphabetically

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,101 +1,57 @@
 return require('packer').startup(function()
      use 'wbthomason/packer.nvim'
-     -- Editor Functionality
-     use 'easymotion/vim-easymotion'
-     use 'mhinz/vim-startify'
-     use 'ryanoasis/vim-devicons'
-     use { 'junegunn/fzf', run = './install --bin' }
-     use 'junegunn/fzf.vim'
-     use 'ctrlpvim/ctrlp.vim'
-     use 'nvim-lua/plenary.nvim'
-     use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } } }
-     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-     use { "nvim-telescope/telescope-file-browser.nvim" }
-     use 'voldikss/vim-floaterm'
-     use 'cljoly/telescope-repo.nvim'
-     use 'kdheepak/lazygit.nvim'
-     use 'karoliskoncevicius/vim-sendtowindow'
-     use 'jpalardy/vim-slime'
-     -- Editing Related
-     use 'Raimondi/delimitMate'
-     use 'godlygeek/tabular'
-     use 'plasticboy/vim-markdown'
-     use 'mattn/webapi-vim'
-     use 'Yggdroot/indentLine'
-     use 'tpope/vim-surround'
-     use 'mattn/emmet-vim'
-     use 'wellle/targets.vim'
-     use 'vim-syntastic/syntastic'
-     use 'sheerun/vim-polyglot'
-     use({
-          "iamcco/markdown-preview.nvim",
-          run = ":call mkdp#util#install()",
-          ft = { "markdown", "packer" },
-     })
-     use 'junegunn/vim-emoji'
+     use 'airblade/vim-gitgutter'
      use 'christoomey/vim-system-copy'
-     use 'dhruvasagar/vim-table-mode'
-     use 'ferrine/md-img-paste.vim'
-     --use 'SidOfc/mkdx'
-     use 'weirongxu/plantuml-previewer.vim'
-     use 'tyru/open-browser.vim'
+     use 'cljoly/telescope-repo.nvim'
+     use 'ctrlpvim/ctrlp.vim'
      use 'dhruvasagar/vim-open-url'
+     use 'dhruvasagar/vim-table-mode'
+     use 'easymotion/vim-easymotion'
+     use 'EdenEast/nightfox.nvim'
+     use 'fatih/vim-go'
+     use 'ferrine/md-img-paste.vim'
+     use 'godlygeek/tabular'
+     use 'hashivim/vim-terraform'
+     use { 'hrsh7th/cmp-vsnip', requires = { 'hrsh7th/vim-vsnip', 'rafamadriz/friendly-snippets', } }
+     use { 'hrsh7th/nvim-cmp', requires = { 'nvim-treesitter', 'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline', } }
      use 'hrsh7th/vim-vsnip'
      use 'hrsh7th/vim-vsnip-integ'
-     use 'sakshamgupta05/vim-todo-highlight'
-     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-     use 'preservim/nerdcommenter'
-     use "lukas-reineke/indent-blankline.nvim"
-     -- Programming
-     -- Language Server installer
-     use {
-          "williamboman/nvim-lsp-installer",
-          {
-               "neovim/nvim-lspconfig",
-          }
-     }
-     -- Show VSCode-esque pictograms
-     use 'onsails/lspkind-nvim'
-     use { 'tami5/lspsaga.nvim', requires = { 'neovim/nvim-lspconfig' } }
-     -- Autocompletion plugin
-     use {
-          'hrsh7th/nvim-cmp',
-          requires = {
-               'nvim-treesitter',
-               'hrsh7th/cmp-nvim-lsp',
-               'hrsh7th/cmp-buffer',
-               'hrsh7th/cmp-path',
-               'hrsh7th/cmp-cmdline',
-          }
-     }
-     -- GitHub pull requests
-     use {
-          'ldelossa/gh.nvim',
-          requires = { { 'ldelossa/litee.nvim' } }
-     }
-     -- snippets
-     use {
-          'hrsh7th/cmp-vsnip', requires = {
-               'hrsh7th/vim-vsnip',
-               'rafamadriz/friendly-snippets',
-          }
-     }
-     -- Fancier statusline
-     use {
-          'nvim-lualine/lualine.nvim',
-          requires = {
-               'kyazdani42/nvim-web-devicons',
-               'arkav/lualine-lsp-progress',
-          },
-     }
+     use 'jpalardy/vim-slime'
+     use { 'junegunn/fzf', run = './install --bin' }
+     use 'junegunn/fzf.vim'
+     use 'junegunn/vim-emoji'
+     use 'kdheepak/lazygit.nvim'
+     use { 'ldelossa/gh.nvim', requires = { { 'ldelossa/litee.nvim' } } }
+     use 'lukas-reineke/indent-blankline.nvim'
      use 'majutsushi/tagbar'
-     use 'hashivim/vim-terraform'
-     use 'fatih/vim-go'
+     use 'mattn/emmet-vim'
+     use 'mattn/webapi-vim'
+     use 'mhinz/vim-startify'
      use { 'neoclide/coc.nvim', branch = 'release' }
-     use 'rhysd/vim-clang-format'
-     use 'tpope/vim-fugitive'
-     use 'airblade/vim-gitgutter'
-     -- Color Schemes
      use 'NLKNguyen/papercolor-theme'
-     use "EdenEast/nightfox.nvim" -- Packer
+     use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', 'arkav/lualine-lsp-progress', }, }
+     use 'nvim-lua/plenary.nvim'
+     use { 'nvim-telescope/telescope-file-browser.nvim' }
+     use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+     use { 'nvim-telescope/telescope.nvim', requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } } }
+     use 'onsails/lspkind-nvim'
+     use 'plasticboy/vim-markdown'
+     use 'preservim/nerdcommenter'
+     use 'Raimondi/delimitMate'
+     use 'rhysd/vim-clang-format'
+     use 'ryanoasis/vim-devicons'
+     use 'sakshamgupta05/vim-todo-highlight'
+     use 'sheerun/vim-polyglot'
+     use { 'tami5/lspsaga.nvim', requires = { 'neovim/nvim-lspconfig' } }
+     use 'tpope/vim-fugitive'
+     use 'tpope/vim-surround'
+     use 'tyru/open-browser.vim'
+     use 'vim-syntastic/syntastic'
+     use 'voldikss/vim-floaterm'
+     use 'weirongxu/plantuml-previewer.vim'
+     use 'wellle/targets.vim'
+     use { 'williamboman/nvim-lsp-installer', { 'neovim/nvim-lspconfig', } }
+     use 'Yggdroot/indentLine'
+     use({ 'iamcco/markdown-preview.nvim', run = ':call mkdp#util#install()', ft = { 'markdown', 'packer' }, })
+     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 end)
