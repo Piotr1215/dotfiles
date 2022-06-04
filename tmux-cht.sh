f4a8eba250f6661b5085f1b6e34df8277cec969b
Piotr Zaniewski <piotr@upbound.io>
2022-06-04 17:05:53 +0200

    Pipe language query to bat

--- tmux-cht.sh
+++ tmux-cht.sh
@@ -10,6 +10,6 @@ if grep -qs "$selected" ~/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
     tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | batcat --paging=always"
 fi
 
