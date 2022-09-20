a9df55bd1c7e6fa4f6cba2562a83b8d6beee081e
Piotr Zaniewski <piotr@upbound.io>
2022-09-20 12:27:25 +0200

    Don't use `` for interpolation

--- tmux-cht.sh
+++ tmux-cht.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+selected=$(cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf)
 if [[ -z $selected ]]; then
     exit 0
 fi
