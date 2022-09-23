0d8365360ce34eef0ab77fd188d7935161a92014
Piotr Zaniewski <piotr@upbound.io>
2022-09-23 17:36:21 +0200

    Cleaning up markdown settings after moving to dedicated settings file

--- lua/settings.lua
+++ lua/settings.lua
@@ -160,9 +160,6 @@ vim.g.go_highlight_structs = 1
 vim.g.go_highlight_interfaces = 1
 vim.g.go_highlight_operators = 1
 vim.g.go_fmt_experimental = 1
--- MarkdownPreview settings
-vim.g.mkdp_browser = '/usr/bin/google-chrome'
-vim.g.mkdp_echo_preview_url = 0
 -- Nerdcommenter
 vim.g.NERDSpaceDelims = 1
 -- Ranger
