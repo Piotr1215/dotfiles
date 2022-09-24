ba6e4c139b12453b1dda2737fdc6cad34a0daffa
Piotr Zaniewski <piotr@upbound.io>
2022-09-24 17:45:54 +0200

    Plugins cleanup
    -

--- .config/nvim/lua/telescope-setup.lua
+++ .config/nvim/lua/telescope-setup.lua
@@ -13,7 +13,6 @@ require('telescope').setup {
 require('telescope').load_extension('file_browser')
 require('telescope').load_extension('repo')
 require('telescope').load_extension('fzf')
-require('telescope').load_extension('projects')
 require("telescope").load_extension("recent_files")
 require("telescope").load_extension("emoji")
 
