50455dceb913005158fa29ade5661f57a6eb9a6c
Piotr Zaniewski <piotr@upbound.io>
2022-09-24 17:45:40 +0200

    YAML specific ftplugin

---
+++ .config/nvim/ftplugin/yaml.lua
@@ -0,0 +1,26 @@
+vim.api.nvim_buf_set_keymap(0, "n", "<leader>yt", ":YAMLTelescope<CR>", { noremap = false })
+
+-- Setup cmp setup buffer configuration - 👻 text off for markdown
+local cmp = require "cmp"
+cmp.setup.buffer {
+  sources = {
+    { name = "vsnip" },
+    { name = "spell" },
+    {
+      name = "buffer",
+      option = {
+        get_bufnrs = function()
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            bufs[vim.api.nvim_win_get_buf(win)] = true
+          end
+          return vim.tbl_keys(bufs)
+        end,
+      },
+    },
+    { name = "path" },
+  },
+  experimental = {
+    ghost_text = false,
+  },
+}
