7f3e2708cec59dd4766dd5079d6f8ccac81cb167
Piotr Zaniewski <piotr@upbound.io>
2022-09-27 20:40:20 +0200

    Swapping to moonfly theme as default, all themes are transparent by default too, tr to change

--- .config/nvim/lua/settings/colorschemes.lua
+++ .config/nvim/lua/settings/colorschemes.lua
@@ -34,5 +34,39 @@ let g:PaperColor_Theme_Options = {
   \ }
 ]]
 
+require("transparent").setup({
+  enable = true, -- boolean: enable transparent
+  extra_groups = { -- table/string: additional groups that should be cleared
+    -- In particular, when you set it to 'all', that means all available groups
+
+    -- example of akinsho/nvim-bufferline.lua
+    "BufferLineTabClose",
+    "BufferlineBufferSelected",
+    "BufferLineFill",
+    "BufferLineBackground",
+    "BufferLineSeparator",
+    "BufferLineIndicatorSelected",
+  },
+  exclude = {}, -- table: groups you don't want to clear
+})
+
+-- Moonfly
+local winhighlight = {
+  winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel",
+}
+local cmp = require('cmp')
+require('cmp').setup({
+  window = {
+    completion = cmp.config.window.bordered(winhighlight),
+    documentation = cmp.config.window.bordered(winhighlight),
+  }
+})
+
+vim.g.moonflyNormalFloat = true
+vim.g.moonflyWinSeparator = 2
+vim.g.moonflyTransparent = true
+vim.opt.fillchars = { horiz = '━', horizup = '┻', horizdown = '┳', vert = '┃', vertleft = '┫', vertright = '┣', verthoriz = '╋', }
+vim.g.termigurcolors = true
 -- Active colorscheme
-vim.cmd('colorscheme nightfox')
+vim.cmd [[colorscheme moonfly]]
+-- vim.cmd('colorscheme nightfox')
