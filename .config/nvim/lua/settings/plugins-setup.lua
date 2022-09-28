790bc99ee08c9536e6a2a4ef1ae6382fe31884ae
Piotr Zaniewski <piotr@upbound.io>
2022-09-28 20:04:47 +0200

    More plugins cleanup

--- .config/nvim/lua/settings/plugins-setup.lua
+++ .config/nvim/lua/settings/plugins-setup.lua
@@ -29,6 +29,17 @@ require("nvim-tree").setup({
 
 local prettier = require("prettier")
 
+require("which-key").setup({
+  plugins = {
+    marks = true, -- shows a list of your marks on ' and `
+    registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+    spelling = {
+      enabled = true, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+      suggestions = 20, -- how many suggestions should be shown in the list?
+    },
+  }
+})
+
 prettier.setup({
   bin = 'prettier', -- or `'prettierd'` (v0.22+)
   filetypes = {
