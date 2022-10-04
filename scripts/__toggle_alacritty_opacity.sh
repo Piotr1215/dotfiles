8adfbc55c37f99eb773709dd707fa04f5480f7a1
Piotr Zaniewski <piotr@upbound.io>
2022-10-04 21:19:02 +0200

    Simple script to toggle window opacity for alacritty

---
+++ scripts/__toggle_alacritty_opacity.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# The set -e option instructs bash to immediately exit if any command has a non-zero exit status
+# The set -u referencing a previously undefined  variable - with the exceptions of $* and $@ - is an error
+# The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
+# https://bit.ly/37nFgin
+set -euo pipefail
+
+# Add source and line number wher running in debug mode: bash -xv test.sh
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+# Set new line and tab for word splitting
+IFS=$'\n\t'
+
+config_file="$HOME/.config/alacritty/alacritty.yml"
+
+case "$(/usr/bin/yq eval '.window.opacity' "$config_file")" in
+0.9)
+	sed -i "s#  opacity: \(.*\)#  opacity: 1.0#" "$config_file"
+	;;
+1.0)
+	sed -i "s#  opacity: \(.*\)#  opacity: 0.9#" "$config_file"
+	;;
+*)
+	sed -i "s#  opacity: \(.*\)#  opacity: 0.9#" "$config_file"
+	;;
+esac
