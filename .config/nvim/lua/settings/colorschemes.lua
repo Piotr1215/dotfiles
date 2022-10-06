3e5192a289d97a78ce6f18495c13367793260bfa
Piotr Zaniewski <piotr@upbound.io>
2022-10-06 13:36:25 +0200

    Moving away from opacity

--- .config/nvim/lua/settings/colorschemes.lua
+++ .config/nvim/lua/settings/colorschemes.lua
@@ -35,7 +35,7 @@ let g:PaperColor_Theme_Options = {
 ]]
 
 require("transparent").setup({
-  enable = true, -- boolean: enable transparent
+  enable = false, -- boolean: enable transparent
   extra_groups = { -- table/string: additional groups that should be cleared
     -- In particular, when you set it to 'all', that means all available groups
     -- example of akinsho/nvim-bufferline.lua
@@ -68,5 +68,6 @@ vim.g.moonflyTransparent = true
 vim.opt.fillchars = { horiz = '━', horizup = '┻', horizdown = '┳', vert = '┃', vertleft = '┫', vertright = '┣', verthoriz = '╋', }
 vim.g.termigurcolors = true
 -- Active colorscheme
-vim.cmd [[colorscheme moonfly]]
+-- vim.cmd [[colorscheme moonfly]]
 -- vim.cmd('colorscheme nightfox')
+vim.cmd('colorscheme tokyonight-storm')
