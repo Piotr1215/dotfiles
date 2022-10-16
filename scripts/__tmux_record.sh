cd6812e903b1d7adad98a32fb93839368298823d
Piotr Zaniewski <piotr@upbound.io>
2022-10-16 18:00:22 +0200

    Better tmux recorder

--- scripts/__tmux_record.sh
+++ scripts/__tmux_record.sh
@@ -5,9 +5,7 @@
 # The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
 # https://bit.ly/37nFgin
 set -euo pipefail
-# Set new line and tab for word splitting
-IFS=$'\n\t'
 
-session=$(tmux display-message -p '#S')
+session=$(tmux list-sessions | awk '{print $1}' | tr -d ":" | fzf)
 tmux detach -s "$session"
 asciinema rec --command "tmux attach -t $session" "$PWD"/tmux-$(date +%F--%H%M).cast
