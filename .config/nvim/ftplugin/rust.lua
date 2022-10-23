27e6e9ff9bb09c161eacbe84d1e792d4b349cc27
Piotr Zaniewski <piotr@upbound.io>
2022-10-23 22:18:42 +0200

    Format rust files before save

--- .config/nvim/ftplugin/rust.lua
+++ .config/nvim/ftplugin/rust.lua
@@ -1,2 +1,8 @@
 vim.cmd('set completeopt=menuone,noinsert,noselect')
 vim.cmd('set shortmess+=c')
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = { "*.rs" },
+  command = "RustFmt",
+})
+
