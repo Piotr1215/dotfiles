c16cbe9fb3070040cd1fc37b202ddc924d92f41c
Piotr Zaniewski <piotr@upbound.io>
2022-10-28 14:38:07 +0200

    More pleasent rust development experience

--- .config/nvim/ftplugin/rust.lua
+++ .config/nvim/ftplugin/rust.lua
@@ -1,6 +1,10 @@
 vim.cmd('set completeopt=menuone,noinsert,noselect')
 vim.cmd('set shortmess+=c')
 
+-- Treesitter folding 
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   pattern = { "*.rs" },
   command = "RustFmt",
