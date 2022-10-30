da5787323f9563665c4320271f4c2ddc0e5ae4ba
Piotr Zaniewski <piotr@upbound.io>
2022-10-30 15:24:51 +0100

    Fixing incorrect nvim-tree call

--- .config/nvim/lua/settings/plugins-setup.lua
+++ .config/nvim/lua/settings/plugins-setup.lua
@@ -86,6 +86,20 @@ require("nvim-tree").setup({
     enable = true,
     update_cwd = true
   },
+  view = {
+    float = {
+      enable = true,
+      quit_on_focus_loss = true,
+      open_win_config = {
+        relative = "editor",
+        border = "rounded",
+        width = 30,
+        height = 30,
+        row = 1,
+        col = 1,
+      },
+    },
+  },
 })
 
 local prettier = require("prettier")
