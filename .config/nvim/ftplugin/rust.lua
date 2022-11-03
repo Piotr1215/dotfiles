a4924a3b95e218c5d974c3eeab28dbe394bc1033
Piotr Zaniewski <piotr@upbound.io>
2022-11-03 22:14:19 +0100

    Don't fold rust files by default

--- .config/nvim/ftplugin/rust.lua
+++ .config/nvim/ftplugin/rust.lua
@@ -2,8 +2,8 @@ vim.cmd('set completeopt=menuone,noinsert,noselect')
 vim.cmd('set shortmess+=c')
 
 -- Treesitter folding 
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+-- vim.wo.foldmethod = 'expr'
+-- vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
 
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   pattern = { "*.rs" },
