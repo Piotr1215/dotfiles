8f40cdca1c06a2e9d77b6ada3668188dd346edcf
Piotr Zaniewski <piotr@upbound.io>
2022-11-06 15:27:54 +0100

    LSP Saga key bindings for rust-tools

--- .config/nvim/lua/lsp-setup.lua
+++ .config/nvim/lua/lsp-setup.lua
@@ -66,7 +66,7 @@ local on_attach = function(_, bufnr)
   -- Rename symbol
   buf_set_keymap('n', '<leader>rn', "<cmd>lua require('lspsaga.rename').rename()<CR>", opts)
 
-  -- Find references
+  -- Find reference
   buf_set_keymap('n', 'gr', '<cmd>lua require("lspsaga.provider").lsp_finder()<CR>', opts)
 
   -- Doc popup scrolling
@@ -153,6 +153,9 @@ rt.setup({
     -- how to execute terminal commands
     -- options right now: termopen / quickfix
     executor = require("rust-tools/executors").quickfix,
+    hover_actions = {
+      auto_focus = true,
+    },
   },
   server = {
     settings = {
@@ -170,6 +173,24 @@ rt.setup({
       vim.keymap.set("n", "<leader>ar", rt.hover_actions.hover_actions, { buffer = bufnr })
       -- Code action groups
       vim.keymap.set("n", "<leader>ag", rt.code_action_group.code_action_group, { buffer = bufnr })
+      vim.keymap.set('n', 'K', require('lspsaga.hover').render_hover_doc, { buffer = bufnr })
+      -- jump to definition
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr })
+      -- Rename symbol
+      vim.keymap.set('n', '<leader>rn', require('lspsaga.rename').rename, { buffer = bufnr })
+      --       Vim commands to move through diagnostics.
+      vim.keymap.set('n', 'gj', ':Lspsaga diagnostic_jump_prev<CR>', { buffer = bufnr })
+      vim.keymap.set('n', 'gk', ':Lspsaga diagnostic_jump_next<CR>', { buffer = bufnr })
+
+      -- Find reference
+      vim.keymap.set('n', 'gr', require("lspsaga.provider").lsp_finder, { buffer = bufnr })
+
+      -- codeaction
+      vim.keymap.set('n', '<leader>ac', require('lspsaga.codeaction').code_action, { buffer = bufnr })
+      vim.keymap.set('v', '<leader>a', require('lspsaga.codeaction').range_code_action, { buffer = bufnr })
+
+      -- breakpoint
+      vim.keymap.set('n', '<leader>tb', require('dap').toggle_breakpoint, { buffer = bufnr })
     end,
   },
 })
