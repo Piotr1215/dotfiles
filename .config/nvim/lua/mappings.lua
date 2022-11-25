f9a91d2bf70a4c308d9c2884ecefeb14f63cb28f
Piotr Zaniewski <piotr@upbound.io>
2022-11-25 10:23:33 +0100

    Mappings for quitting and save-quitting nvim combinable with the moonlander macros

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,6 +6,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- MOVE AROUND --
+utils.lnmap("wa", ":wqa<cr>")
+utils.lnmap("qa", ":qa!<cr>")
 -- j/k moves over virtual (wrapped) lines
 vim.api.nvim_set_keymap('n', 'k', "v:count == 0 ? 'gk' : 'k'", { noremap = true, expr = true, silent = true })
 vim.api.nvim_set_keymap('n', 'j', "v:count == 0 ? 'gj' : 'j'", { noremap = true, expr = true, silent = true })
