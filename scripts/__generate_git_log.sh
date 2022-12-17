0ff90d5be42b2744668b527ae025bdd940850582
Piotr Zaniewski <piotr@upbound.io>
2022-12-17 13:46:11 +0100

    Changelog

--- scripts/__generate_git_log.sh
+++ scripts/__generate_git_log.sh
@@ -4,7 +4,7 @@
 # The set -u referencing a previously undefined  variable - with the exceptions of $* and $@ - is an error
 # The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
 # https://bit.ly/37nFgin
-set -euo pipefail
+set -eu
 
 # Add source and line number wher running in debug mode: bash -xv __generate_git_log.sh
 export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
@@ -16,4 +16,4 @@ IFS=$'\n\t'
 repo=$(basename $(git rev-parse --show-toplevel))
 
 # Take last 10 commits and wrap them in https
-git log --pretty="%H| - %s" | grep -v Merge | grep -v chore | head -n 10 | sed 's/\(.*|\)/[&]\(https:\/\/github.com\/piotr1215\/'$repo'\/&\)/g' | sed 's/|//g'
+git log --pretty="%H| - %s" | grep -v Merge | grep -v chore | head -n 10 | sed 's/\(.*|\)/- [&]\(https:\/\/github.com\/piotr1215\/'$repo'\/&\)/g' | sed 's/|//g'
