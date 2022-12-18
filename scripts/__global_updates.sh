6d43ff3017c136ea95e55852e892813f82df6e1f
Piotr Zaniewski <piotr@upbound.io>
2022-12-18 12:32:19 +0100

    Script to do cargo and go global packages updates

---
+++ scripts/__global_updates.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# The set -e option instructs bash to immediately exit if any command has a non-zero exit status
+# The set -u referencing a previously undefined  variable - with the exceptions of $* and $@ - is an error
+# The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
+# https://bit.ly/37nFgin
+set -euo pipefail
+
+# Add source and line number wher running in debug mode: bash -xv __global_updates.sh
+export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+# Set new line and tab for word splitting
+IFS=$'\n\t'
+
+source ./__check_root.sh
+
+go-global-update
+cargo install-update -a
