81ec9499269855a41dfa824d5a27d5b261dfdead
Piotr Zaniewski <piotr@upbound.io>
2023-01-07 11:58:15 +0100

    nvim.oil
    This plugin enables treating a file system as a vim buffer. Things like
    motions, text objects, search and replace etc, all apply to the bufer.
    This makes the file system edit operations "vim idiomatic".

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -6,6 +6,7 @@ return require('packer').startup(function(use)
     "windwp/nvim-autopairs",
     config = function() require("nvim-autopairs").setup {} end
   }
+  use 'stevearc/oil.nvim'
   use 'echasnovski/mini.nvim'
   use 'sheerun/vim-polyglot'
   use 'mattn/emmet-vim'
