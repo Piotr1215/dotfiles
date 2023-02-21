c3d429b339418fd1167a5b1c6c2fc2c6f0468e37
Piotr Zaniewski <piotr@upbound.io>
2023-02-21 10:49:47 +0100

    Spring color scheme

--- .config/nvim/lua/settings/colorschemes.lua
+++ .config/nvim/lua/settings/colorschemes.lua
@@ -70,4 +70,4 @@ vim.g.termigurcolors = true
 -- Active colorscheme
 -- vim.cmd [[colorscheme moonfly]]
 -- vim.cmd('colorscheme nightfox')
-vim.cmd('colorscheme catppuccin')
+vim.cmd('colorscheme tokyonight-moon')
