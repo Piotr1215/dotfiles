27d8c606d05fe87f7949053654e1fb3db69454ca
Piotr Zaniewski <piotr@upbound.io>
2023-02-26 15:07:01 +0100

    Install fixes

--- install.sh
+++ install.sh
@@ -99,6 +99,7 @@ process "→ install exa"
 EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
 curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
 sudo unzip -q exa.zip bin/exa -d /usr/local
+sudo rm exa.zip
 
 process "→ install go"
 
