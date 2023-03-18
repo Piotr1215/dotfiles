b27b29099d948003b361cdc3d1b923f18a8981c4
Piotr Zaniewski <piotr@upbound.io>
2023-03-18 18:18:42 +0100

    Now can open up to 4 files in vim in checker splits

--- scripts/__open-file.sh
+++ scripts/__open-file.sh
@@ -1,18 +1,32 @@
 #!/usr/bin/env bash
 
+source __trap.sh
+
 # The set -e option instructs bash to immediately exit if any command has a non-zero exit status
 # The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
 # https://bit.ly/37nFgin
 set -eo pipefail
 
 # Add source and line number wher running in debug mode: bash -xv __open-file.sh
-export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
 
 # Set new line and tab for word splitting
 IFS=$'\n' files=($(fzf-tmux --preview "bat --color=always {$1}" --reverse --query="$1" --multi --select-1 --exit-0))
-if [[ "${#files[@]}" -gt 2 ]]; then
 
-	[[ -n "$files" ]] && ${EDITOR:-nvim} -p "${files[@]}"
-else
-	[[ -n "$files" ]] && ${EDITOR:-nvim} -O "${files[@]}"
-fi
+for i in "${!files[@]}"; do
+	files[i]=$(realpath "${files[i]}")
+done
+
+case "${#files[@]}" in
+2)
+	${EDITOR:-nvim} -O "${files[@]}"
+	;;
+3)
+	${EDITOR:-nvim} -O "${files[0]}" -c 'wincmd j' -c "vsplit ${files[1]}" -c "split ${files[2]}"
+	;;
+4)
+	${EDITOR:-nvim} -O "${files[0]}" -c "vsplit ${files[1]}" -c "split ${files[2]}" -c 'wincmd h' -c "split ${files[3]}"
+	;;
+*)
+	echo "Invalid number of files selected"
+	;;
+esac
