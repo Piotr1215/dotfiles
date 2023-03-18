c9a9e8c5f8ff3ee585ec4aa047a9c31f67270478
Piotr Zaniewski <piotr@upbound.io>
2023-03-18 18:18:25 +0100

    Tracing and error handling of bash scripts

--- scripts/__trap.sh
+++ scripts/__trap.sh
@@ -2,10 +2,10 @@
 
 # Define a function to print the error message and exit with a non-zero status
 print_error() {
-	echo "ERROR: An error occurred in the script \"$0\" on line $1" >>error-pipe
-	cat error-pipe >&2
+	local line_number="$1"
+	echo "ERROR: An error occurred in the script \"$0\" on line $line_number" >&2
 	exit 1
 }
 
 # Set the error trap
-trap 'print_error $? $LINENO' ERR
+trap 'print_error $LINENO' ERR
