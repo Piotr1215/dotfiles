0f67317f2e06b81f2f16ce2c5c097f9b9fe8610d
Piotr Zaniewski <piotr@upbound.io>
2023-03-18 18:27:35 +0100

    Cleanup

--- scripts/__open-file.sh
+++ scripts/__open-file.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+# Source generic error handling funcion
 source __trap.sh
 
 # The set -e option instructs bash to immediately exit if any command has a non-zero exit status
+# The set -u referencing a previously undefined  variable - with the exceptions of $* and $@ - is an error
 # The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
 # https://bit.ly/37nFgin
 set -eo pipefail
 
-# Add source and line number wher running in debug mode: bash -xv __open-file.sh
+# Add source and line number wher running in debug mode: __run_with_xtrace.sh __open-file.sh
 
 # Set new line and tab for word splitting
 IFS=$'\n' files=($(fzf-tmux --preview "bat --color=always {$1}" --reverse --query="$1" --multi --select-1 --exit-0))
@@ -27,6 +29,6 @@ case "${#files[@]}" in
 	${EDITOR:-nvim} -O "${files[0]}" -c "vsplit ${files[1]}" -c "split ${files[2]}" -c 'wincmd h' -c "split ${files[3]}"
 	;;
 *)
-	echo "Invalid number of files selected"
+	${EDITOR:-nvim} "${files[@]}"
 	;;
 esac
