483158bbb178e13b73978058f60fe3149ed4496f
Piotr Zaniewski <piotr@upbound.io>
2023-03-18 11:48:22 +0100

    Adjusting the helper functions to acommodate for additonal optional params

--- .config/nvim/lua/utils.lua
+++ .config/nvim/lua/utils.lua
@@ -1,7 +1,9 @@
 local M = {}
 
-local function map(mode, shortcut, command)
-  vim.api.nvim_set_keymap(mode, shortcut, command, { noremap = true, silent = true })
+local function map(mode, shortcut, command, options)
+  local default_options = { noremap = true, silent = true }
+  local merged_options = options and vim.tbl_extend('force', default_options, options) or default_options
+  vim.api.nvim_set_keymap(mode, shortcut, command, merged_options)
 end
 
 function M.emap(shortcut, command)
@@ -9,37 +11,37 @@ function M.emap(shortcut, command)
 end
 
 --Leader normal mapping
-function M.lnmap(shortcut, command)
+function M.lnmap(shortcut, command, options)
   local leader = "<leader>" .. shortcut
-  map('n', leader, command)
+  map('n', leader, command, options)
 end
 
-function M.nmap(shortcut, command)
-  map('n', shortcut, command)
+function M.nmap(shortcut, command, options)
+  map('n', shortcut, command, options)
 end
 
-function M.imap(shortcut, command)
-  map('i', shortcut, command)
+function M.imap(shortcut, command, options)
+  map('i', shortcut, command, options)
 end
 
-function M.vmap(shortcut, command)
-  map('v', shortcut, command)
+function M.vmap(shortcut, command, options)
+  map('v', shortcut, command, options)
 end
 
-function M.xmap(shortcut, command)
-  map('x', shortcut, command)
+function M.xmap(shortcut, command, options)
+  map('x', shortcut, command, options)
 end
 
-function M.omap(shortcut, command)
-  map('o', shortcut, command)
+function M.omap(shortcut, command, options)
+  map('o', shortcut, command, options)
 end
 
-function M.smap(shortcut, command)
-  map('s', shortcut, command)
+function M.smap(shortcut, command, options)
+  map('s', shortcut, command, options)
 end
 
-function M.tmap(shortcut, command)
-  map('t', shortcut, command)
+function M.tmap(shortcut, command, options)
+  map('t', shortcut, command, options)
 end
 
 return M
