3326a035304b0fd7aa70f6179344ca66b600b00d
Piotr Zaniewski <piotr@upbound.io>
2023-03-18 22:29:17 +0100

    Making trace script more robust

--- scripts/__run_with_xtrace.sh
+++ scripts/__run_with_xtrace.sh
@@ -1,9 +1,32 @@
 #!/usr/bin/env bash
 
+# The set -e option instructs bash to immediately exit if any command has a non-zero exit status
+# The set -u referencing a previously undefined  variable - with the exceptions of $* and $@ - is an error
+# The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
+# https://bit.ly/37nFgin
+set -eo pipefail
+
+# Set new line and tab for word splitting
+IFS=$'\n\t'
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+	echo "Usage: $0 <script_or_function> [args...]"
+	echo "Runs the given script or function with the bash '-x' option to show debug information."
+	exit 0
+fi
+
 if [ -z "$1" ]; then
 	echo "Usage: $0 <script_or_function> [args...]"
 	exit 1
 fi
+
+function_exists() {
+	declare -F "$1" >/dev/null
+}
+
+script_exists() {
+	[ -f "$(which "$1")" ] && [ -x "$(which "$1")" ]
+}
 
 input_to_run="$1"
 shift
@@ -14,9 +37,9 @@ echo -e "${header}"
 
 export PS4='+(${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): } \t '
 
-if [ -f "$input_to_run" ]; then
+if script_exists "$input_to_run"; then
 	BASH_XTRACEFD=2 bash -x "$input_to_run" "$@"
-else
+elif function_exists "$input_to_run"; then
 	temp_script=$(mktemp)
 
 	echo "source ~/.zsh_functions" >>"$temp_script"
@@ -25,4 +48,7 @@ else
 	BASH_XTRACEFD=2 bash -x "$temp_script" "$@"
 
 	rm "$temp_script"
+else
+	echo "Error: '$input_to_run' not found as a script or function. Please provide a valid script file or function name."
+	exit 1
 fi
