7ead6dd0dbc06078b14f9b4ad2b8ab657a7da279
Piotr Zaniewski <piotr@upbound.io>
2023-03-30 11:45:21 +0200

    Make openfile a bit more robust and Ctrl+j to jump to zoxide dirs

--- scripts/__open-file.sh
+++ scripts/__open-file.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env bash
 
-# Source generic error handling funcion
+# Source generic error handling function
 source __trap.sh
 
 # The set -e option instructs bash to immediately exit if any command has a non-zero exit status
-# The set -u referencing a previously undefined  variable - with the exceptions of $* and $@ - is an error
+# The set -u referencing a previously undefined variable - with the exceptions of $* and $@ - is an error
 # The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
 # https://bit.ly/37nFgin
-set -eo pipefail
+set -o pipefail
 
-# Add source and line number wher running in debug mode: __run_with_xtrace.sh __open-file.sh
+# Custom error handling function for fzf-tmux
+handle_fzf_error() {
+	if [ $? -eq 130 ]; then
+		# If fzf-tmux was interrupted by Ctrl+C (exit code 130), exit gracefully
+		exit 0
+	else
+		# Otherwise, re-raise the error
+		return $?
+	fi
+}
+
+# Add source and line number when running in debug mode: __run_with_xtrace.sh __open-file.sh
 
 # Set new line and tab for word splitting
-IFS=$'\n' files=($(fzf-tmux --preview "bat --color=always {$1}" --reverse --query="$1" --multi --select-1 --exit-0))
+IFS=$'\n' files=($(fzf-tmux --preview "bat --color=always {$1}" --reverse --query="$1" --multi --select-1 --exit-0 || handle_fzf_error))
+
+# Check if any files were selected, and exit if not
+if [ ${#files[@]} -eq 0 ]; then
+	exit 0
+fi
 
 for i in "${!files[@]}"; do
 	files[i]=$(realpath "${files[i]}")
