5a7fe6257120d46ac0d04740fe664c4c7b89223d
Piotr Zaniewski <piotr@upbound.io>
2023-04-24 16:28:54 +0200

    Switching to neovim stable, due to some glitches

--- scripts/__upgrade-nvim.sh
+++ scripts/__upgrade-nvim.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-#curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+# curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 sudo chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/local/bin/nvim
