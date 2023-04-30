664bf2f30e7ebe88f89559200b6c1550a1b35b54
Piotr Zaniewski <piotr@upbound.io>
2023-04-30 21:56:46 +0200

    Red

--- scripts/__download_youtube.sh
+++ scripts/__download_youtube.sh
@@ -4,7 +4,6 @@
 source __trap.sh
 
 # The set -e option instructs bash to immediately exit if any command has a non-zero exit status
-# The set -u referencing a previously undefined  variable - with the exceptions of $* and $@ - is an error
 # The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
 # https://bit.ly/37nFgin
 set -eo pipefail
