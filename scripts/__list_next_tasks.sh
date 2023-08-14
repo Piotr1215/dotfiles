ef29b6eabbf7cc7426e3a7d70b1a91b1273ebf50
Piotr Zaniewski <piotr@upbound.io>
2023-08-14 10:03:51 +0200

    Treat multilines well

--- scripts/__list_next_tasks.sh
+++ scripts/__list_next_tasks.sh
@@ -9,6 +9,9 @@ cleaned_output=$(echo "$output" | sed '1,2d;$d' | grep -vE '^[- ]*$')
 
 # Process the cleaned output
 declare -A tasks
+last_project=""
+append_to_last=false
+
 while IFS= read -r line; do
 	# Extract project and description
 	project="${line%% *}"
@@ -17,13 +20,22 @@ while IFS= read -r line; do
 	# Trim leading spaces from the description
 	description=$(echo "$description" | sed 's/^[ \t]*//')
 
-	# Check if the line is empty (indicating a new project)
-	if [ -z "$line" ]; then
-		continue
+	# If the project is not in the project_descriptions, it's a continuation line
+	if [[ ! ${project_descriptions["$project"]} ]]; then
+		project="$last_project"
+		append_to_last=true
+	else
+		last_project="$project"
+		append_to_last=false
 	fi
 
 	# Append to the tasks associative array
-	tasks["$project"]+="- [ ] $description"$'\n'
+	if [ "$append_to_last" = true ]; then
+		# Remove the last newline and append the continuation line
+		tasks["$project"]="${tasks["$project"]%$'\n'} $description"$'\n'
+	else
+		tasks["$project"]+="- [ ] $description"$'\n'
+	fi
 done <<<"$cleaned_output"
 
 # Print the tasks
