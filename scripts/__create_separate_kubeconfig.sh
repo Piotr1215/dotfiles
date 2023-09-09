909a5f448e858ec4f0b6498b9758bd4e616d9fee
Piotr Zaniewski <piotr@upbound.io>
2023-09-09 20:20:55 +0200

    Helper for create kubeconfig

--- scripts/__create_separate_kubeconfig.sh
+++ scripts/__create_separate_kubeconfig.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Check if a folder name was provided
+# If not, output a message and exit the script
 if [ -z "$1" ]; then
 	echo "Please provide the name of the folder to create."
 	exit 1
 fi
 
-# Create the folder
+# Create the folder with the provided name
 mkdir -p "$1"
 
+# Create a new 'config' file in the created folder
 touch "$1/config"
 
 # Create the .envrc file with the desired content
+# This file will be used to set environment variables when the directory is entered
 cat <<EOL >"$1/.envrc"
 source_up
 KUBECONFIG=./config
