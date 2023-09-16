61173b1f86237ffda068df637f643a0069c2317a
Piotr Zaniewski <piotr@upbound.io>
2023-09-16 11:19:53 +0200

    Watch and sync AutoKey scripts without soft linking all the things

---
+++ scripts/__sync_autokey.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# The set -e option instructs bash to immediately exit if any command has a non-zero exit status
+# The set -u referencing a previously undefined  variable - with the exceptions of $* and $@ - is an error
+# The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
+# https://bit.ly/37nFgin
+set -eo pipefail
+
+cd /home/decoder/dev/dotfiles
+while true; do
+	inotify-hookable \
+		--watch-files ~/.config/autokey/data/Sample\ Scripts/ \
+		--on-modify-command "rsync -av --exclude='*.json' ~/.config/autokey/data/Sample\ Scripts/ ~/dev/dotfiles/.config/autokey/data/Sample\ Scripts/"
+done
