4064685f63594529dabb8e87b2edbe512c8ef904
Piotr Zaniewski <piotr@upbound.io>
2023-09-18 22:12:03 +0200

    Add ability to select obsidian note to annotate the task with

---
+++ .config/taskwarrior-tui/shortcut-scripts/__annotate_with_note.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# UUID of the task to annotate
+uuid="$1"
+
+# Directory where notes are stored
+notes_dir="/home/decoder/dev/obsidian/decoder/Notes"
+
+# Use fzf to select a note file
+filepath=$(find "$notes_dir" -type f -name '*.md' | fzf-tmux --preview "bat --color=always {}")
+
+# If fzf was cancelled or no file was selected, exit the script
+if [ -z "$filepath" ]; then
+	echo "No file selected. Exiting."
+	exit 1
+fi
+
+# Annotate the task with the selected filepath
+task_output=$(task "$uuid" annotate "$filepath")
+
+# Check if annotation was successful
+if [[ "$task_output" == *"Annotated"* ]]; then
+	echo "Successfully annotated the task with the note."
+else
+	echo "Failed to annotate the task."
+fi
