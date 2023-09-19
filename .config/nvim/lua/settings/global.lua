065f38af9dc9fb99cf049aeb4acc0f47b71c74b6
Piotr Zaniewski <piotr@upbound.io>
2023-09-19 21:16:50 +0200

    Treesitter folding, hover text on LSP, faster completion options

--- .config/nvim/lua/settings/global.lua
+++ .config/nvim/lua/settings/global.lua
@@ -10,22 +10,36 @@ vcmd('set winbar=%=%m%F')
 vcmd('set completefunc=emoji#complete')
 vcmd('set wildignore+=*/tmp/*,*.so,*.swp,*.zip')
 vcmd('set backspace=indent,eol,start')
-vcmd('set foldexpr=getline(v:lnum)=~\'^\\s*$\'&&getline(v:lnum+1)=~\'\\S\'?\'<1\':1')
 vcmd('set jumpoptions=view')
 vcmd('set sessionoptions+=tabpages,globals')
-vim.o.completeopt = 'menuone,noselect'
+
+-- Treesitter folding
+vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.api.nvim_set_option('updatetime', 300)
+
+-- Fixed column for diagnostics to appear
+-- Show autodiagnostic popup on cursor hover_range
+-- Goto previous / next diagnostic warning / error
+-- Show inlay_hints more frequently
+vim.cmd([[
+set signcolumn=yes
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
 
 --Remap for dealing with word wrap
+set.completeopt = { 'menuone', 'noselect', 'noinsert' }
+set.shortmess = set.shortmess + { c = true }
 set.background = 'dark'
 set.ignorecase = true -- ignore case in search
-set.smartcase = true -- do not ignore case with capitals
+set.smartcase = true  -- do not ignore case with capitals
 set.scrolloff = 8
 set.hlsearch = true
 set.updatetime = 300
 set.splitright = true -- put new splits to the right
 set.splitbelow = true -- put new splits below
 set.lazyredraw = true -- do not redraw for macros, faster execution
-set.undofile = true -- persistent undo even after session close
+set.undofile = true   -- persistent undo even after session close
 
 set.number = true
 set.encoding = "utf-8"
