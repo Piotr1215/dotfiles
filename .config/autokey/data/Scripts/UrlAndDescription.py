5d2b4119bc904f17f1d26dbb26f1c4c7dca643dd
Piotr Zaniewski <piotr@upbound.io>
2023-09-19 17:19:32 +0200

    Add automated tagging based on page content

--- .config/autokey/data/Scripts/UrlAndDescription.py
+++ .config/autokey/data/Scripts/UrlAndDescription.py
@@ -2,24 +2,86 @@ import subprocess
 import time
 import pexpect
 from urllib.parse import urlparse
+import os
+from bs4 import BeautifulSoup
+import requests
+import openai
 
+from dotenv import load_dotenv
+envrc_path = os.path.expanduser('~/.envrc')
+with open(envrc_path, 'r') as f:
+    lines = f.readlines()
+
+for line in lines:
+    if line.startswith('export '):
+        key_value_pair = line[7:].strip().split('=')
+        if len(key_value_pair) == 2:
+            key, value = key_value_pair
+            os.environ[key] = value
+
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+def sanitize_tags(raw_tags):
+    if not isinstance(raw_tags, list):
+        return ["+unknown"]
+    
+    sanitized_tags = []
+    for tag in raw_tags:
+        clean_tag = tag.replace("+", "")
+        if clean_tag.isalnum():
+            sanitized_tags.append(f"+{clean_tag}")
+        else:
+            return ["+unknown"]
+
+    return sanitized_tags if sanitized_tags else ["+unknown"]
+
+    
+def get_tags_from_openai(description):
+    prompt = f"Based on the provided website description: '{description}', please generate a list of up to 5 relevant tags for categorizing the content. Tags should have a maximum of 14 characters, include no special characters, and be words like +linux, +shopping, +pets. Please format the tags like this: +tag1, +tag2, +tag3, ..."
+    
+    chat_completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{
+            "role": "user",
+            "content": prompt
+        }]
+    )
+    
+    tags_output = chat_completion['choices'][0]['message']['content'] # type: ignore
+    # Sanitize output (make sure it only includes the tags)
+    tags = tags_output.strip().split(", ")
+    return sanitize_tags(tags)
+
+def get_website_description(url):
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        meta_tag = soup.find('meta', {'name': 'description'}) or soup.find('meta', {'property': 'og:description'})
+
+        if meta_tag:
+            return meta_tag['content'] # type: ignore
+        else:
+            return "No description available"
+    except Exception as e:
+        return f"An error occurred: {e}"
+        
 # Function to get custom task descriptions
 def get_custom_description(default_description):
-    command = f"zenity --entry --text 'Enter task description:' --title 'Task Description' --entry-text '{default_description}'"
+    command = f"zenity --entry --text 'Enter task description:' --title 'Task Description' --entry-text '{default_description}' --width 400"
     try:
         task_description = subprocess.check_output(command, shell=True, text=True).strip()
         return task_description
     except subprocess.CalledProcessError:
         return None
 
-def invoke_plink(description, url):
+def invoke_plink(description, url, tags):
     if not url.startswith('http://') and not url.startswith('https://'):
         print("Not a valid URL.")
         return 1
 
     command_name = f"xdg-open \"{url}\""
     command_description = f"Link to {description}"
-    command_tag = "link"
+    command_tag = ",".join(tag[1:] for tag in tags)  # Remove the "+" from each tag and join them
 
     child = pexpect.spawn('pet new -t')
     child.expect('Command>')
@@ -50,13 +112,14 @@ if 'Firefox' in active_window_title:
     time.sleep(0.5)  # Allow some time for the clipboard operation to complete
     
     url = clipboard.get_clipboard()
-
+    website_description = get_website_description(url)
+    tags = get_tags_from_openai(website_description) 
     # If no text is selected, use the window title as the description
     if not description:
         description = active_window_title
     
     # Invoke plink function with the captured description and URL
-    invoke_plink(description, url)
+    invoke_plink(description, url, tags)
     #message = "The url '{}' and description '{}' were added".format(url, description)
     #dialog.info_dialog(title="Information", message=message)
     # Create a custom dialog box with Zenity
@@ -75,7 +138,7 @@ if 'Firefox' in active_window_title:
 
         # If the custom description is provided, use it; otherwise, use the existing description logic
         description = custom_description if custom_description else description
-        subprocess.run(["/home/decoder/dev/dotfiles/scripts/__create_task.sh", description, "+idea"])
+        subprocess.run(["/home/decoder/dev/dotfiles/scripts/__create_task.sh", description] + tags)
 
     clipboard.fill_clipboard("")
     clipboard.fill_selection("")
