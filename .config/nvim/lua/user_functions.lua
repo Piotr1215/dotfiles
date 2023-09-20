5efbfcc0ed65303cf0a2e6876c78d0774884b62d
Piotr Zaniewski <piotr@upbound.io>
2023-09-20 21:44:06 +0200

    Remove old user function that was converted to plugin

--- .config/nvim/lua/user_functions.lua
+++ .config/nvim/lua/user_functions.lua
@@ -1,14 +1,14 @@
 -- TODO: consider splitting the whole file into smaller parts
-local wk = require("which-key")
+local wk = require "which-key"
 
 function _G.print_current_file_dir()
-  local dir = vim.fn.expand('%:p:h')
-  if dir ~= '' then
+  local dir = vim.fn.expand "%:p:h"
+  if dir ~= "" then
     print(dir)
   end
 end
 
-vim.cmd("command! GetCurrentFileDir lua print_current_file_dir()")
+vim.cmd "command! GetCurrentFileDir lua print_current_file_dir()"
 
 -- Store the zoom state
 local zoomed = false
@@ -16,21 +16,21 @@ local zoomed = false
 -- Function to toggle zoom
 function _G.toggle_zoom()
   if zoomed then
-    vim.api.nvim_command('wincmd =')
+    vim.api.nvim_command "wincmd ="
     zoomed = false
   else
-    vim.api.nvim_command('wincmd _')
-    vim.api.nvim_command('wincmd |')
+    vim.api.nvim_command "wincmd _"
+    vim.api.nvim_command "wincmd |"
     zoomed = true
   end
 end
 
 -- Key mapping
-vim.api.nvim_set_keymap('n', '<leader>zw', ':lua toggle_zoom()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>zw", ":lua toggle_zoom()<CR>", { noremap = true, silent = true })
 
 function _G.ranger_popup_in_tmux()
   -- Get the directory of the current file in Neovim
-  local current_file = vim.fn.expand('%:p:h')
+  local current_file = vim.fn.expand "%:p:h"
 
   -- Formulate the tmux command with either the file directory or the pane's current path
   local tmux_command = "tmux popup -d '" .. current_file .. "' -E -h 95% -w 95% -x 100% 'ranger'"
@@ -39,7 +39,7 @@ function _G.ranger_popup_in_tmux()
   os.execute(tmux_command)
 end
 
-vim.api.nvim_set_keymap('n', '<leader>mr', ':lua ranger_popup_in_tmux()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>mr", ":lua ranger_popup_in_tmux()<CR>", { noremap = true, silent = true })
 
 function _G.go_to_task_in_taskwarrior_tui()
   -- Get the current line and save it as the original line
@@ -49,7 +49,7 @@ function _G.go_to_task_in_taskwarrior_tui()
   vim.cmd [[execute "normal \<Plug>NERDCommenterUncomment"]]
   local uncommented_line = vim.api.nvim_get_current_line()
 
-  local patterns = { 'TODO:', 'HACK:', 'NOTE:', 'PERF:', 'TEST:', 'WARN:' }
+  local patterns = { "TODO:", "HACK:", "NOTE:", "PERF:", "TEST:", "WARN:" }
   local taskDescription = nil
 
   for _, pattern in ipairs(patterns) do
@@ -72,8 +72,12 @@ function _G.go_to_task_in_taskwarrior_tui()
   vim.api.nvim_set_current_line(original_line)
 end
 
-vim.api.nvim_set_keymap('n', '<leader>gt', [[<Cmd>lua go_to_task_in_taskwarrior_tui()<CR>]],
-  { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>gt",
+  [[<Cmd>lua go_to_task_in_taskwarrior_tui()<CR>]],
+  { noremap = true, silent = true }
+)
 
 function _G.mark_task_done()
   -- Get the current line and parse it
@@ -85,7 +89,7 @@ function _G.mark_task_done()
   line = vim.api.nvim_get_current_line()
   print("Uncommented line: ", line)
 
-  local patterns = { 'TODO:', 'HACK:', 'NOTE:', 'PERF:', 'TEST:', 'WARN:' }
+  local patterns = { "TODO:", "HACK:", "NOTE:", "PERF:", "TEST:", "WARN:" }
   local taskDescription = nil
   for _, pattern in ipairs(patterns) do
     local start_idx = string.find(line, pattern)
@@ -109,13 +113,13 @@ function _G.mark_task_done()
 end
 
 -- Map the function to a key
-vim.api.nvim_set_keymap('n', '<leader>dt', [[<Cmd>lua mark_task_done()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>dt", [[<Cmd>lua mark_task_done()<CR>]], { noremap = true, silent = true })
 -- Function for creating or updating a Taskwarrior task
 function _G.create_or_update_task()
-  local current_line = vim.fn.getline('.')
-  local cursor_pos = vim.fn.col('.')
-  local file_path = vim.fn.expand('%:p') -- Get full path of current file
-  local line_number = vim.fn.line('.')   -- Get current line number
+  local current_line = vim.fn.getline "."
+  local cursor_pos = vim.fn.col "."
+  local file_path = vim.fn.expand "%:p" -- Get full path of current file
+  local line_number = vim.fn.line "." -- Get current line number
 
   -- Keywords we are looking for
   local keywords = { "TODO", "HACK", "NOTE", "PERF", "TEST", "WARN" }
@@ -127,17 +131,17 @@ function _G.create_or_update_task()
       local task_tag = "+" .. string.lower(keyword)
 
       -- Ask for project and other tags
-      local project = vim.fn.input('Enter project name: ')
-      local additional_tags_input = vim.fn.input('Enter additional tags separated by spaces: ')
+      local project = vim.fn.input "Enter project name: "
+      local additional_tags_input = vim.fn.input "Enter additional tags separated by spaces: "
       local additional_tags = {}
 
       -- Prefix each additional tag with a "+"
-      for tag in additional_tags_input:gmatch("%S+") do
+      for tag in additional_tags_input:gmatch "%S+" do
         table.insert(additional_tags, "+" .. tag)
       end
 
       -- Prepare the task command
-      local task_cmd = string.format("task add %s \"%s\"", task_tag, task_description)
+      local task_cmd = string.format('task add %s "%s"', task_tag, task_description)
 
       -- Add additional tags if available
       if #additional_tags > 0 then
@@ -153,20 +157,20 @@ function _G.create_or_update_task()
       local output = vim.fn.system(task_cmd)
       print("Output: ", output)
 
-      for line in output:gmatch("[^\r\n]+") do
+      for line in output:gmatch "[^\r\n]+" do
         local task_id = string.match(line, "Created task (%d+)%.")
         if task_id then
           print("Task ID extracted: ", task_id)
 
           -- Annotate task with filename and line number in the nvimline format
           local annotation = string.format("nvimline:%s:%s", line_number, file_path)
-          local annotate_cmd = string.format("task %s annotate \"%s\"", task_id, annotation)
+          local annotate_cmd = string.format('task %s annotate "%s"', task_id, annotation)
           local annotate_output = vim.fn.system(annotate_cmd)
 
           print("Annotation output: ", annotate_output)
           return
         else
-          print("Failed to extract task ID")
+          print "Failed to extract task ID"
         end
       end
     end
@@ -174,102 +178,82 @@ function _G.create_or_update_task()
 end
 
 -- Bind Ctrl-T in insert mode to call the create_or_update_task function
-vim.api.nvim_set_keymap('i', '<C-T>', [[<Cmd>lua create_or_update_task(vim.fn.getline('.'))<CR>]],
-  { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "i",
+  "<C-T>",
+  [[<Cmd>lua create_or_update_task(vim.fn.getline('.'))<CR>]],
+  { noremap = true, silent = true }
+)
 
-local actions = require('telescope.actions')
-local action_state = require('telescope.actions.state')
+local actions = require "telescope.actions"
+local action_state = require "telescope.actions.state"
 
 function _G.insert_file_path()
-  require('telescope.builtin').find_files({
-    cwd = '~/dev', -- Set the directory to search
+  require("telescope.builtin").find_files {
+    cwd = "~/dev", -- Set the directory to search
     attach_mappings = function(_, map)
-      map('i', '<CR>', function(prompt_bufnr)
+      map("i", "<CR>", function(prompt_bufnr)
         local selected_file = action_state.get_selected_entry(prompt_bufnr).path
         actions.close(prompt_bufnr)
 
         -- Replace the home directory with ~
-        selected_file = selected_file:gsub(vim.fn.expand("$HOME"), "~")
+        selected_file = selected_file:gsub(vim.fn.expand "$HOME", "~")
 
         -- Ask the user if they want to insert the full path or just the file name
-        local choice = vim.fn.input("Insert full path or file name? (n[ame]/p[ath]): ")
+        local choice = vim.fn.input "Insert full path or file name? (n[ame]/p[ath]): "
         local text_to_insert
-        if choice == 'p' then
+        if choice == "p" then
           text_to_insert = selected_file
-        elseif choice == 'n' then
-          text_to_insert = vim.fn.fnamemodify(selected_file, ':t')
+        elseif choice == "n" then
+          text_to_insert = vim.fn.fnamemodify(selected_file, ":t")
         end
 
         -- Move the cursor back one position if it's between quotes
-        local col = vim.fn.col('.') - 1
-        if vim.fn.getline('.')[col] == "'" or vim.fn.getline('.')[col] == '"' then
-          vim.fn.cursor(vim.fn.line('.'), col)
+        local col = vim.fn.col "." - 1
+        if vim.fn.getline(".")[col] == "'" or vim.fn.getline(".")[col] == '"' then
+          vim.fn.cursor(vim.fn.line ".", col)
         end
 
         -- Insert the text at the cursor position
-        vim.api.nvim_put({ text_to_insert }, 'c', true, true)
+        vim.api.nvim_put({ text_to_insert }, "c", true, true)
       end)
       return true
     end,
-  })
+  }
 end
 
-vim.api.nvim_set_keymap('i', '<M-i>', [[<Cmd>lua insert_file_path()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<M-i>", [[<Cmd>lua insert_file_path()<CR>]], { noremap = true, silent = true })
 
 function _G.toggle_function_folding()
   if vim.wo.foldenable and vim.wo.foldmethod == "expr" then
-    print("Disabling folding")
-    vim.cmd('setlocal nofoldenable')
-    vim.cmd('setlocal foldmethod=manual')
+    print "Disabling folding"
+    vim.cmd "setlocal nofoldenable"
+    vim.cmd "setlocal foldmethod=manual"
   else
-    print("Enabling folding")
-    vim.cmd('setlocal foldenable')
-    vim.cmd('setlocal foldmethod=expr')
-    vim.cmd([[setlocal foldexpr=getline(v:lnum)=~'^function\\s\\+\\w\\+\\s*()'?'a1':getline(v:lnum)=~'}'?'s1':'=']])
+    print "Enabling folding"
+    vim.cmd "setlocal foldenable"
+    vim.cmd "setlocal foldmethod=expr"
+    vim.cmd [[setlocal foldexpr=getline(v:lnum)=~'^function\\s\\+\\w\\+\\s*()'?'a1':getline(v:lnum)=~'}'?'s1':'=']]
   end
 end
 
-vim.cmd("command! Fold lua _G.toggle_function_folding()")
-
---[[ function _G.yank_matching_lines() ]]
---[[ local search_pattern = vim.fn.getreg('/') ]]
---[[ if search_pattern ~= '' then ]]
---[[ local matching_lines = {} ]]
---[[ for line_number = 1, vim.fn.line('$') do ]]
---[[ local line = vim.fn.getline(line_number) ]]
---[[ if vim.fn.match(line, search_pattern) ~= -1 then ]]
---[[ table.insert(matching_lines, line) ]]
---[[ end ]]
---[[ end ]]
---[[ if #matching_lines > 0 then ]]
---[[ local original_filetype = vim.bo.filetype ]]
---[[ vim.fn.setreg('+', table.concat(matching_lines, '\n')) ]]
---[[ vim.cmd('new') ]]
---[[ vim.cmd('0put +') ]]
---[[ vim.bo.filetype = original_filetype ]]
---[[ else ]]
---[[ print("No matches found") ]]
---[[ end ]]
---[[ end ]]
---[[ end ]]
-
--- vim.api.nvim_set_keymap('n', '<Leader>ya', ':lua _G.yank_matching_lines()<CR>', { noremap = true, silent = true })
+vim.cmd "command! Fold lua _G.toggle_function_folding()"
 
 function _G.create_word_selection_mappings()
   for i = 2, 5 do
     local count = 2 * i - 1
-    vim.api.nvim_set_keymap('n', 'v' .. i, 'v' .. count .. 'iw', { noremap = true })
-    wk.register({ ['v' .. i] = { 'v' .. count .. 'iw', 'Select ' .. i .. ' words' } }, { mode = 'n', prefix = '' })
+    vim.api.nvim_set_keymap("n", "v" .. i, "v" .. count .. "iw", { noremap = true })
+    wk.register({ ["v" .. i] = { "v" .. count .. "iw", "Select " .. i .. " words" } }, { mode = "n", prefix = "" })
   end
-  vim.api.nvim_set_keymap('n', '_', 'vg_', { noremap = true })
-  wk.register({ ['_'] = { 'vg_', 'Select inside underscored word' } }, { mode = 'n', prefix = '' })
+  vim.api.nvim_set_keymap("n", "_", "vg_", { noremap = true })
+  wk.register({ ["_"] = { "vg_", "Select inside underscored word" } }, { mode = "n", prefix = "" })
 end
 
 create_word_selection_mappings()
 
 -- Custom f command function
 -- This is needed because ;; is mapped to enter command mode
-vim.cmd([[
+vim.cmd [[
 function! CustomF(backwards)
   let l:char = nr2char(getchar())
   if a:backwards
@@ -280,7 +264,7 @@ function! CustomF(backwards)
   nnoremap ; ;
   vnoremap ; ;
 endfunction
-]])
+]]
 
 -- Map f to the custom f command function so that pressing f and ; works as expected
 vim.api.nvim_set_keymap("n", "f", ":call CustomF(0)<CR>", {})
@@ -310,38 +294,43 @@ function _G.process_task_list(...)
   local new_lines = {}
 
   -- Adding shebang and setting options
-  table.insert(new_lines, '#!/usr/bin/env bash')
-  table.insert(new_lines, 'set -eo pipefail')
+  table.insert(new_lines, "#!/usr/bin/env bash")
+  table.insert(new_lines, "set -eo pipefail")
 
   for _, line in ipairs(lines) do
-    local trimmed_line = line:gsub('^[•*%-%+]+%s*', '') -- Remove bullet points
+    local trimmed_line = line:gsub("^[•*%-%+]+%s*", "") -- Remove bullet points
     local links = {}
 
     -- Extract http/https links and remove them from the task description
-    trimmed_line = trimmed_line:gsub('(https?://[%w%.%-%_/&%?=%~]+)', function(link)
+    trimmed_line = trimmed_line:gsub("(https?://[%w%.%-%_/&%?=%~]+)", function(link)
       table.insert(links, link)
-      return ''
+      return ""
     end)
 
     if #trimmed_line > 0 then
-      table.insert(new_lines,
-        'output=$(task add ' .. (modifiers ~= '' and modifiers .. ' ' or '') .. '"' .. trimmed_line .. '")')
-      table.insert(new_lines, 'task_id=$(echo "$output" | grep -o "Created task [0-9]*." | cut -d " " -f 3 | tr -d ".")')
+      table.insert(
+        new_lines,
+        "output=$(task add " .. (modifiers ~= "" and modifiers .. " " or "") .. '"' .. trimmed_line .. '")'
+      )
+      table.insert(
+        new_lines,
+        'task_id=$(echo "$output" | grep -o "Created task [0-9]*." | cut -d " " -f 3 | tr -d ".")'
+      )
 
       -- Annotate the task with the extracted links
       for _, link in ipairs(links) do
-        table.insert(new_lines, 'task $task_id annotate -- ' .. link)
+        table.insert(new_lines, "task $task_id annotate -- " .. link)
       end
     else
-      table.insert(new_lines, '') -- Keep empty lines
+      table.insert(new_lines, "") -- Keep empty lines
     end
   end
 
   vim.api.nvim_buf_set_lines(0, 0, -1, false, new_lines)
 end
 
-vim.api.nvim_set_keymap('n', '<Leader>pt', ':lua _G.process_task_list()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>pt", ":lua _G.process_task_list()<CR>", { noremap = true, silent = true })
 
-vim.cmd([[
+vim.cmd [[
   command! -nargs=* -complete=customlist,v:lua.my_custom_complete ProcessTasks :lua _G.process_task_list(<f-args>)
-]])
+]]
