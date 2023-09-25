b526021f3360b067e4a591293f4d4c795a4ae775
Piotr Zaniewski <piotr@upbound.io>
2023-09-25 22:40:41 +0200

    Finally fixing the fold command! Now fld toggles fold on and off

--- .config/nvim/lua/settings/global.lua
+++ .config/nvim/lua/settings/global.lua
@@ -1,45 +1,47 @@
 local set = vim.opt
 local vcmd = vim.cmd
 
-vcmd('syntax on')
-vcmd('syntax enable')
-vcmd('filetype on')
-vcmd('filetype plugin indent on')
-vcmd('set clipboard=unnamedplus')
-vcmd('set winbar=%=%m%F')
-vcmd('set completefunc=emoji#complete')
-vcmd('set wildignore+=*/tmp/*,*.so,*.swp,*.zip')
-vcmd('set backspace=indent,eol,start')
-vcmd('set jumpoptions=view')
-vcmd('set sessionoptions+=tabpages,globals')
+vcmd "syntax on"
+vcmd "syntax enable"
+vcmd "filetype on"
+vcmd "filetype plugin indent on"
+vcmd "set clipboard=unnamedplus"
+vcmd "set winbar=%=%m%F"
+vcmd "set completefunc=emoji#complete"
+vcmd "set wildignore+=*/tmp/*,*.so,*.swp,*.zip"
+vcmd "set backspace=indent,eol,start"
+vcmd "set jumpoptions=view"
+vcmd "set sessionoptions+=tabpages,globals"
 
 -- Treesitter folding
-vim.wo.foldmethod = 'expr'
-vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.api.nvim_set_option('updatetime', 300)
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.wo.foldenable = false
+vcmd "setlocal nofoldenable"
+vim.api.nvim_set_option("updatetime", 300)
 
 -- Fixed column for diagnostics to appear
 -- Show autodiagnostic popup on cursor hover_range
 -- Goto previous / next diagnostic warning / error
 -- Show inlay_hints more frequently
-vim.cmd([[
+vim.cmd [[
 set signcolumn=yes
 autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
-]])
+]]
 
 --Remap for dealing with word wrap
-set.completeopt = { 'menuone', 'noselect', 'noinsert' }
+set.completeopt = { "menuone", "noselect", "noinsert" }
 set.shortmess = set.shortmess + { c = true }
-set.background = 'dark'
+set.background = "dark"
 set.ignorecase = true -- ignore case in search
-set.smartcase = true  -- do not ignore case with capitals
+set.smartcase = true -- do not ignore case with capitals
 set.scrolloff = 8
 set.hlsearch = true
 set.updatetime = 300
 set.splitright = true -- put new splits to the right
 set.splitbelow = true -- put new splits below
 set.lazyredraw = true -- do not redraw for macros, faster execution
-set.undofile = true   -- persistent undo even after session close
+set.undofile = true -- persistent undo even after session close
 
 set.number = true
 set.encoding = "utf-8"
