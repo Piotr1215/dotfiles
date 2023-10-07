08a44d96c068b20dc1ebc11954843807cba0c085
Piotr Zaniewski <piotr@upbound.io>
2023-10-07 12:01:09 +0200

    Custom Projects Completion
    Adding abililty to use TAG in comments to create relationships between
    various files that belong to the same `project`. List of projects comes
    from a custom completion source and TAG is highlighted as part of the
    todo plugin.
    
    This creates well integrated experience with the ability to use
    Telescope and other builtin neovim features to manage related projects
    as tags in otherwise decentralized files.

---
+++ .config/nvim/lua/tags/module.lua
@@ -0,0 +1,47 @@
+local source = {}
+
+---Return whether this source is available in the current context or not (optional).
+---@return boolean
+function source:is_available()
+  return true
+end
+
+---Return the debug name of this source (optional).
+---@return string
+function source:get_debug_name()
+  return 'tags'
+end
+
+---Return the keyword pattern for triggering completion (optional).
+---If this is ommited, nvim-cmp will use a default keyword pattern. See |cmp-config.completion.keyword_pattern|.
+---@return string
+function source:get_keyword_pattern()
+  return [[\k\+]]
+end
+
+---Invoke completion (required).
+---@param params cmp.SourceCompletionApiParams
+---@param callback fun(response: lsp.CompletionResponse|nil)
+function source:complete(params, callback)
+  callback({
+    { label = 'ranger-tmux-setup' },
+  })
+end
+
+---Resolve completion item (optional). This is called right before the completion is about to be displayed.
+---Useful for setting the text shown in the documentation window (`completion_item.documentation`).
+---@param completion_item lsp.CompletionItem
+---@param callback fun(completion_item: lsp.CompletionItem|nil)
+function source:resolve(completion_item, callback)
+  callback(completion_item)
+end
+
+---Executed after the item was selected.
+---@param completion_item lsp.CompletionItem
+---@param callback fun(completion_item: lsp.CompletionItem|nil)
+function source:execute(completion_item, callback)
+  callback(completion_item)
+end
+
+---Register your source to nvim-cmp.
+require('cmp').register_source('tags', source)
