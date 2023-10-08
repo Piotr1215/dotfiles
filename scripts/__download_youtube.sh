0985be9ef6f381a9046274f6ebc734faa00e94ac
Piotr Zaniewski <piotr@upbound.io>
2023-10-08 17:42:52 +0200

    Make ydl download videos by default and with -mp3 only music

--- scripts/__download_youtube.sh
+++ scripts/__download_youtube.sh
@@ -1,25 +1,40 @@
 #!/usr/bin/env bash
 
-# Source generic error handling function
-source __trap.sh
-
 set -eo pipefail
 IFS=$'\n\t'
+
+# Check for -mp3 flag
+convert_to_mp3=false
+if [[ "$1" == "-mp3" ]]; then
+	convert_to_mp3=true
+fi
 
 echo "Step 1: Checking the link format."
 
 link=$(xsel -ob)
 if [[ "$link" != *"youtu"* ]]; then
 	echo "This is not the right format, copy again."
-	return 1 2>/dev/null
+	exit 1
 fi
 
-echo "Step 2: Downloading the video."
+# Determine the output directory based on the flag
+output_dir="$HOME/Videos"
+if [ "$convert_to_mp3" = true ]; then
+	output_dir="$HOME/music"
+fi
 
-yt-dlp -o "~/music/%(title)s.%(ext)s" --merge-output-format webm "$link" --no-playlist
+echo "Step 2: Downloading the video to $output_dir."
+
+yt-dlp -o "$output_dir/%(title)s.%(ext)s" --merge-output-format webm "$link" --no-playlist
 
 video_title=$(yt-dlp --get-filename -o '%(title)s' --no-playlist "$link")
-video_file=$(find ~/music -type f -iname "*${video_title}*" | head -n 1)
+video_file=$(find "$output_dir" -type f -iname "*${video_title}*" | head -n 1)
+
+# If -mp3 flag is not passed, exit after downloading
+if [ "$convert_to_mp3" = false ]; then
+	echo "Video downloaded. Exiting."
+	exit 0
+fi
 
 echo "Step 3: Preparing for MP3 conversion."
 
@@ -28,12 +43,12 @@ mp3_file="${video_file%.webm}.mp3"
 echo "Step 4: Converting to MP3."
 
 if ffmpeg -i "$video_file" -vn -ar 44100 -ac 2 -b:a 192k "$mp3_file"; then
-	echo "Step 5: Removing the video file."
+	echo "Step 5: Removing the original video file."
 	rm "$video_file"
-	echo "Video converted to MP3 and video file removed."
+	echo "Video converted to MP3 and original video file removed."
 else
 	echo "Conversion failed."
-	return 1 2>/dev/null
+	exit 1
 fi
 
 echo "Step 6: Renaming the MP3 file."
