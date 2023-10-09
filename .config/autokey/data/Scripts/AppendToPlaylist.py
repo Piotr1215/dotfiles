aa5646639ae4795342c24286fbf97cee79e9e974
Piotr Zaniewski <piotr@upbound.io>
2023-10-09 21:14:48 +0200

    Append vid to playlist for ytd

---
+++ .config/autokey/data/Scripts/AppendToPlaylist.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+
+# Path to the playlist file
+vids_playlist_path = os.path.expanduser('~/vids_playlist.m3u')
+# Path to the Videos folder
+videos_folder_path = os.path.expanduser('~/Videos')
+# Path to the Bash script for downloading YouTube videos
+download_script_path = '/home/decoder/dev/dotfiles/scripts/__download_youtube.sh'
+
+def append_to_playlist(playlist_file_path):
+    url = clipboard.get_clipboard()
+
+    # Read existing URLs from the playlist file
+    existing_urls = []
+    if os.path.exists(playlist_file_path):
+        with open(playlist_file_path, 'r') as f:
+            existing_urls = [line.strip() for line in f.readlines()]
+
+    # Check if the URL is already in the playlist
+    if url not in existing_urls:
+        # Call the Bash script to download the video
+        subprocess.run([download_script_path])
+
+        # Get the video title
+        video_title = subprocess.getoutput(f"yt-dlp --get-filename -o '%(title)s' --no-playlist {url}")
+
+        # Generate the video file path
+        video_file_path = os.path.join(videos_folder_path, f"{video_title}.webm")
+
+        # Append the video file path to the playlist file
+        with open(playlist_file_path, 'a') as f:
+            f.write(video_file_path + '\n')
+        # Show Zenity notification
+        subprocess.run(["zenity", "--notification", f"--text='Video downloaded and added to playlist: {video_title}'"])
+
+# Get the active window title
+active_window_title = subprocess.getoutput("xdotool getactivewindow getwindowname")
+
+# Check if the active window is Firefox
+if 'Firefox' in active_window_title:
+    append_to_playlist(vids_playlist_path)
+    
+    clipboard.fill_clipboard("")
+    clipboard.fill_selection("")
