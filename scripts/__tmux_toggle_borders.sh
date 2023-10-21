025b68849e9327975d2958593c2d4c9ec6c9639c
Piotr Zaniewski <piotr@upbound.io>
2023-10-21 11:06:15 +0200

    Toggle borders for presentation mode

---
+++ scripts/__tmux_toggle_borders.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the current values
+status=$(tmux show-option -gqv status)
+
+# Toggle the values
+if [[ "$status" == "on" ]]; then
+	tmux set-option -g pane-border-status off
+	tmux set-option -g status off
+elif [[ "$status" == "off" ]]; then
+	tmux set-option -g pane-border-status top
+	tmux set-option -g status on
+fi
