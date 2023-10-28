965b68352b1c3f8c01028b481c4f54392428f05b
Piotr Zaniewski <piotr@upbound.io>
2023-10-28 13:29:09 +0200

    Add sleep timer to avoid prepending the calling key

--- .config/autokey/data/Scripts/Insert IT.py
+++ .config/autokey/data/Scripts/Insert IT.py
@@ -20,5 +20,8 @@ markdown_template = '''## Description
 # Set the clipboard content using AutoKey's built-in functionality
 clipboard.fill_clipboard(markdown_template)
 
+# Sleep a little to avoid prepending the ! to text
+time.sleep(0.2)
+
 # Simulate a Shift+Insert to paste it
 keyboard.send_keys("<shift>+<insert>")
