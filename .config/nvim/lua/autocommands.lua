f4f579a56abf83ce2cd21a4b4e4fb2730b02c77f
Piotr Zaniewski <piotr@upbound.io>
2023-10-29 10:12:58 +0100

    Execure any command in scratch buffer

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -156,12 +156,29 @@ vim.api.nvim_create_user_command(
   { bang = true, nargs = "*" }
 )
 
---Execute shell command in a read-only scratchpad buffer
+-- Define a Lua function to create the scratch buffer, execute the shell command, and set the keymap
+function create_scratch_buffer(args)
+  -- Create a new scratch buffer
+  vim.cmd("new")
+  vim.cmd("setlocal buftype=nofile bufhidden=hide noswapfile")
+
+  -- Execute the shell command and capture its output in the buffer
+  vim.cmd("r !" .. args)
+
+  -- Get the buffer number of the current (scratch) buffer
+  local buf = vim.api.nvim_get_current_buf()
+
+  -- Set the 'q' key to exit the buffer in the scratch buffer
+  vim.api.nvim_buf_set_keymap(buf, 'n', 'q', ':q!<CR>', { noremap = true, silent = true })
+end
+
+-- Create a user command 'R' to execute your Lua function, passing along any arguments
 vim.api.nvim_create_user_command(
   "R",
-  "new | setlocal buftype=nofile bufhidden=hide noswapfile | r !<args>",
+  "lua create_scratch_buffer(<q-args>)",
   { bang = false, nargs = "*", complete = "shellcmd" }
 )
+
 
 --Get diff for current file
 vim.api.nvim_create_user_command("Gdiff", "execute  'w !git diff --no-index -- % -'", { bang = false })
