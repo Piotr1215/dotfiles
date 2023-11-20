a0937de3e32c6413d6d281fe082d336fa4a2768f
Piotr Zaniewski <piotr@upbound.io>
2023-11-20 23:09:49 +0100

    This messed up with imports formatting

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -14,12 +14,12 @@ vim.api.nvim_create_user_command(
   { nargs = 1 }
 )
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    vim.lsp.buf.code_action { context = { only = { "source.organizeImports" } }, apply = true }
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- pattern = "*.go",
+-- callback = function()
+-- vim.lsp.buf.code_action { context = { only = { "source.organizeImports" } }, apply = true }
+-- end,
+-- })
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "go" },
