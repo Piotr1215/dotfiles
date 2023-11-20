1274162713f6e0f701b481fb371a6bcd9fd5a1d9
Piotr Zaniewski <piotr@upbound.io>
2023-11-20 23:09:22 +0100

    Little utility to copy selected text and url of a web page as markdown link

---
+++ .config/autokey/data/Scripts/Copy URL in markdown.py
@@ -0,0 +1,12 @@
+import subprocess
+active_window_title = subprocess.getoutput("xdotool getactivewindow getwindowname")
+
+if 'Firefox' in active_window_title or 'Chrome' in active_window_title or 'Brave' in active_window_title:
+    clipboard.fill_clipboard("")  
+    keyboard.send_keys('yy')
+    time.sleep(0.5)
+    description = clipboard.get_selection()
+    url = clipboard.get_clipboard()
+    combined = f"[{description}]({url})"
+
+    clipboard.fill_clipboard(combined)
