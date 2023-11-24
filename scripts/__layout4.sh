3be5aa143cc0599561545f88ceb58aa38dd5ed7b
Piotr Zaniewski <piotr@upbound.io>
2023-11-24 23:41:59 +0100

    No need for alacritty in slack setup, bringing slack vertically

--- scripts/__layout4.sh
+++ scripts/__layout4.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-MIDDLE_SCREEN_POING=1920
-LEFT_SCREEN_CORNER=0
-HALF_SCREEN_WIDTH=1930
-FULL_SCREEN_WIDTH=3860
 
 slack=$(xdotool search --onlyvisible --classname Slack | head -n 1)
 if [[ -z "${slack}" ]]; then
@@ -11,44 +7,32 @@ if [[ -z "${slack}" ]]; then
 	exit 0
 fi
 
-if pgrep -x "alacritty" >/dev/null; then
-	# Get the ID of the first visible Alacritty window
-	window=$(xdotool search --onlyvisible --classname Alacritty | head -n 1)
-	if [ -n "$window" ]; then
-		wmctrl -i -r "$window" -b remove,maximized_vert,maximized_horz
+# Get the ID of the first Firefox window across all workspaces
+slack_window=$(xdotool search --onlyvisible --classname Slack | head -n 1)
+if [ -n "$slack_window" ]; then
 
-		wmctrl -i -r "$window" -e 0,$LEFT_SCREEN_CORNER,0,$HALF_SCREEN_WIDTH,1080
-	else
-		echo "No Alacritty window found."
-	fi
+	wmctrl -i -r "$slack_window" -b remove,maximized_vert,maximized_horz
 
-	# Get the ID of the first Firefox window across all workspaces
-	slack_window=$(xdotool search --onlyvisible --classname Slack | head -n 1)
-	if [ -n "$slack_window" ]; then
-
-		wmctrl -i -r "$slack_window" -b remove,maximized_vert,maximized_horz
-
-		# wmctrl -i -r ID -e 0,left,up,width,height
-		wmctrl -i -r "$slack_window" -e 0,$LEFT_SCREEN_CORNER,1115,$FULL_SCREEN_WIDTH,1010
-
-	else
-		echo "No Slack window found."
-	fi
-	# Get the ID of the first Firefox window across all workspaces
-	firefox_window=$(xdotool search --onlyvisible --classname Navigator | head -n 1)
-	if [ -n "$firefox_window" ]; then
-		# Unmaximize the window
-
-		# Resize and move the Firefox window to the right side of the screen with exact pixel dimensions
-		wmctrl -i -r "$firefox_window" -b remove,maximized_vert,maximized_horz
-
-		# wmctrl -i -r ID -e 0,left,up,width,height
-		wmctrl -i -r "$firefox_window" -e 0,$MIDDLE_SCREEN_POING,0,$HALF_SCREEN_WIDTH,1108
-	else
-		echo "No Firefox window found."
-	fi
+	xdotool windowmap "$slack_window"
+	# Resize and move the window to the left side of the screen with exact pixel dimensions
+	xdotool windowsize "$slack_window" 1920 2128
+	xdotool windowmove "$slack_window" 0 32
 
 else
-	# Launch Alacritty
-	alacritty &
+	echo "No Slack window found."
+fi
+firefox_window=$(xdotool search --onlyvisible --classname Navigator | head -n 1)
+if [ -n "$firefox_window" ]; then
+	# Unmaximize the window
+
+	# Resize and move the Firefox window to the right side of the screen with exact pixel dimensions
+	wmctrl -i -r "$firefox_window" -b remove,maximized_vert,maximized_horz
+	# Assuming the screen resolution width is 3840, half of it is 1920
+	# Adjust the height according to your screen resolution
+	xdotool windowsize "$firefox_window" 1946 2154
+	# Move the Firefox window to the right side of the screen
+	# The x-coordinate is set to 1920 to position the window on the right half of the screen
+	xdotool windowmove "$firefox_window" 1907 21
+else
+	echo "No Firefox window found."
 fi
