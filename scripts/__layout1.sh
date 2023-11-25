6a9b1ceb5b82dcdac786c224882068ca64433235
Piotr Zaniewski <piotr@upbound.io>
2023-11-25 18:59:19 +0100

    Remove redundant window checks

--- scripts/__layout1.sh
+++ scripts/__layout1.sh
@@ -1,29 +1,21 @@
 #!/bin/bash
 
-# layout1.sh
-# Check if Alacritty is running
-if pgrep -x "alacritty" >/dev/null; then
-	# Get the ID of the first visible Alacritty window
-	window=$(xdotool search --onlyvisible --classname Alacritty | head -n 1)
-	if [ -n "$window" ]; then
-		# Unmaximize the window
-		xdotool windowunmap "$window"
+# Get the ID of the first visible Alacritty window
+window=$(xdotool search --onlyvisible --classname Alacritty | head -n 1)
+if [ -n "$window" ]; then
+	# Unmaximize the window
+	xdotool windowunmap "$window"
 
-		# Resize the window
-		xdotool windowmap "$window"
-		# Resize and move the window to the left side of the screen with exact pixel dimensions
+	# Resize the window
+	xdotool windowmap "$window"
+	# Resize and move the window to the left side of the screen with exact pixel dimensions
 
-		wmctrl -i -r "$window" -b add,maximized_vert,maximized_horz
+	wmctrl -i -r "$window" -b add,maximized_vert,maximized_horz
 
-		# wmctrl -r Alacritty -e 0,1920,0,-1,-1
+	# wmctrl -r Alacritty -e 0,1920,0,-1,-1
 
-		xdotool windowraise "$window"
-		xdotool windowactivate --sync "$window"
-	else
-		echo "No Alacritty window found."
-	fi
-
+	xdotool windowraise "$window"
+	xdotool windowactivate --sync "$window"
 else
-	# Launch Alacritty
-	echo "Please launch Alacritty first."
+	echo "No Alacritty window found."
 fi
