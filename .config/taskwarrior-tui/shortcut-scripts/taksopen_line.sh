f9923eb167524f618e5a8368d3eb448c6caf7471
Piotr Zaniewski <piotr@upbound.io>
2023-12-10 21:34:55 +0100

    Files from tasks will open now in a new temporary tmux session which is easier for editing and doesn't block taskwarrior

--- .config/taskwarrior-tui/shortcut-scripts/taksopen_line.sh
+++ .config/taskwarrior-tui/shortcut-scripts/taksopen_line.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Capture the current session name
+current_session=$(tmux display-message -p '#S')
+
+# Sleep for a bit to allow tui to load
+sleep 0.1
 
 # Extracting the line number and file path from the input string
 LINE_NUMBER=$(echo "$1" | awk -F ':' '{print $2}')
@@ -16,5 +21,20 @@ if [ ! -z "$TASK_DESCRIPTION" ]; then
 	fi
 fi
 
-# Opening the file with neovim at the specific line number and highlighting it
-nvim +$LINE_NUMBER "$FILE_PATH"
+# Capture the file name from the file path without the extension
+FILE_NAME=$(basename "$FILE_PATH" | awk -F '.' '{print $1}')
+DIR_NAME=$(dirname "$FILE_PATH")
+
+# Create a new tmux session which opens the file with neovim at the specific line number and highlighting it
+cd "$DIR_NAME" && tmux new-session -d -s "$FILE_NAME" "direnv exec . $SHELL -c 'nvim +$LINE_NUMBER $FILE_PATH'"
+
+# Attach to the new session
+tmux switch-client -t "$FILE_NAME"
+
+# Wait for the session to be closed, either by the user or some other way
+while tmux has-session -t "$FILE_NAME" 2>/dev/null; do
+	sleep 1
+done
+
+# Switch back to the original session
+tmux switch-client -t "$current_session"
