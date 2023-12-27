bbce04395c031de125158022386bec55bee68ea9
Piotr Zaniewski <piotr@upbound.io>
2023-12-27 20:15:51 +0100

    Improving Firefox tabs a(de)ttach process

---
+++ .config/autokey/data/Scripts/DetachAndCompare.py
@@ -0,0 +1,20 @@
+import time
+import os
+import subprocess
+
+active_window_title = subprocess.getoutput("xdotool getactivewindow getwindowname")
+
+if 'Firefox' in active_window_title:
+    try:
+        layout = '/home/decoder/dev/dotfiles/scripts/__layouts.sh'
+        keyboard.send_keys('<shift>+<alt>+d')  # Detaches tab into a new window
+        time.sleep(0.25)  # Wait half a second for the action to complete
+
+        subprocess.run([layout, "3"])
+
+        time.sleep(0.25)  # Wait half a second for the action to complete
+        subprocess.run([layout, "3"])
+    except Exception as e:
+        subprocess.run(['zenity', '--error', '--text', f"An error occurred: {e}"])
+
+
---
+++ .config/autokey/data/Scripts/ReattachAndMax.py
@@ -0,0 +1,17 @@
+import time
+import os
+import subprocess
+
+active_window_title = subprocess.getoutput("xdotool getactivewindow getwindowname")
+
+if 'Firefox' in active_window_title:
+    try:
+        layout = '/home/decoder/dev/dotfiles/scripts/__layouts.sh'
+        keyboard.send_keys('<shift>+<alt>+m')  # Detaches tab into a new window
+        time.sleep(0.25)  # Wait half a second for the action to complete
+
+        subprocess.run([layout, "5"])
+    except Exception as e:
+        subprocess.run(['zenity', '--error', '--text', f"An error occurred: {e}"])
+
+
