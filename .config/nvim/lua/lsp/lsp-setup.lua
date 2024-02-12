e53dc93bf903815e8992dc3b85bbe625ffcbaadf
Piotr <decoder@live.de>
2024-02-12 15:03:59 +0100

    Splitting default lsp conf to separete file and adding pyright setup
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/lsp/lsp-setup.lua
+++ .config/nvim/lua/lsp/lsp-setup.lua
@@ -2,56 +2,12 @@
 require "lspconfig"
 require("nvim-dap-virtual-text").setup()
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities()
-
--- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
-vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
-
-local on_attach = function(_, bufnr)
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = "LSP: " .. desc
-    end
-
-    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-  nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-  nmap("gr", '<cmd>lua require("lspsaga.provider").lsp_finder()<CR>', "Find Reference")
-  nmap("<leader>rn", "<cmd>lua require('lspsaga.rename').rename()<CR>", "Rename Symbol")
-  nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-  nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-  nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-  -- See `:help K` for why this keymap
-  nmap("K", "<cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", "Hover Documentation")
-
-  -- Lesser used LSP functionality
-  nmap("<leader>wA", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-  nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-  nmap("<leader>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, "[W]orkspace [L]ist Folders")
-
-  nmap("<c-f>", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", "Format Buffer")
-
-  nmap("<leader>br", require("dap").toggle_breakpoint, "Toggle Breakpoint")
-  -- nmap("<leader>cac", ":Lspsaga code_action<CR>", "Code Action")
-  vim.keymap.set({ "n", "v" }, "<leader>caa", "<cmd>Lspsaga code_action<CR>", { silent = true })
-end
+local def = require "lsp.default-lsp"
 
 local lspconfig = require "lspconfig"
-lspconfig.pyright.setup {}
 lspconfig.lua_ls.setup {
-  capabilities = capabilities,
-  on_attach = on_attach,
+  capabilities = def.capabilities,
+  on_attach = def.on_attach,
 
   settings = {
     Lua = {
@@ -79,7 +35,7 @@ require("lspconfig")['up'].setup({
   cmd = { "up", "xpls", "serve", "--verbose" },
   filetypes = { "yaml" },
   root_dir = lspconfig.util.root_pattern "crossplane.yaml",
-  on_attach = on_attach,
+  on_attach = def.on_attach,
 })
 
 require 'lspconfig'.bashls.setup {
@@ -88,8 +44,8 @@ require 'lspconfig'.bashls.setup {
 
 require('lspconfig')['yamlls'].setup {
   {
-    on_attach = on_attach,
-    capabilities = capabilities,
+    on_attach = def.on_attach,
+    capabilities = def.capabilities,
     filetypes = { "yaml", "yml" },
     flags = { debounce_test_changes = 150 },
     settings = {
@@ -117,7 +73,7 @@ require('lspconfig')['yamlls'].setup {
 lspconfig.gopls.setup {
   cmd = { "gopls" },
   -- for postfix snippets and analyzers
-  capabilities = capabilities,
+  capabilities = def.capabilities,
   settings = {
     gopls = {
       experimentalPostfixCompletions = true,
@@ -138,5 +94,5 @@ lspconfig.gopls.setup {
       },
     },
   },
-  on_attach = on_attach,
+  on_attach = def.on_attach,
 }
---
+++ .config/nvim/lua/lsp/python-setup.lua
@@ -0,0 +1,81 @@
+require "lspconfig"
+require("nvim-dap-virtual-text").setup()
+
+local def = require "lsp.default-lsp"
+
+local lspconfig = require "lspconfig"
+local util = require 'lspconfig.util'
+
+local root_files = {
+  'pyproject.toml',
+  'setup.py',
+  'setup.cfg',
+  'requirements.txt',
+  'Pipfile',
+  'pyrightconfig.json',
+  '.git',
+}
+local function organize_imports()
+  local params = {
+    command = 'pyright.organizeimports',
+    arguments = { vim.uri_from_bufnr(0) },
+  }
+
+  local clients = vim.lsp.get_active_clients {
+    bufnr = vim.api.nvim_get_current_buf(),
+    name = 'pyright',
+  }
+  for _, client in ipairs(clients) do
+    client.request('workspace/executeCommand', params, nil, 0)
+  end
+end
+local function set_python_path(path)
+  local clients = vim.lsp.get_active_clients {
+    bufnr = vim.api.nvim_get_current_buf(),
+    name = 'pyright',
+  }
+  for _, client in ipairs(clients) do
+    client.config.settings = vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = path } })
+    client.notify('workspace/didChangeConfiguration', { settings = nil })
+  end
+end
+lspconfig.pyright.setup {
+  capabilities = def.capabilities,
+  on_attach = def.on_attach,
+  default_config = {
+    cmd = { 'pyright-langserver', '--stdio' },
+    filetypes = { 'python' },
+    root_dir = function(fname)
+      return util.root_pattern(unpack(root_files))(fname)
+    end,
+    single_file_support = true,
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          useLibraryCodeForTypes = true,
+          diagnosticMode = 'openFilesOnly',
+        },
+      },
+    },
+  },
+  commands = {
+    PyrightOrganizeImports = {
+      organize_imports,
+      description = 'Organize Imports',
+    },
+    PyrightSetPythonPath = {
+      set_python_path,
+      description = 'Reconfigure pyright with the provided python path',
+      nargs = 1,
+      complete = 'file',
+    },
+  },
+  docs = {
+    description = [[
+https://github.com/microsoft/pyright
+
+`pyright`, a static type checker and language server for python
+]],
+  },
+}
