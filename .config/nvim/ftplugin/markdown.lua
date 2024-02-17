4b39bff1537d46268dcbabe0bb608968a97bac9c
Piotr <decoder@live.de>
2024-02-17 13:28:20 +0100

    markdown: more intuitive movement between headers
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -47,8 +47,8 @@ utils.xmap('am', '<Cmd>lua MarkdownCodeBlock(true)<CR>')
 utils.omap('im', '<Cmd>lua MarkdownCodeBlock(false)<CR>')
 utils.xmap('im', '<Cmd>lua MarkdownCodeBlock(false)<CR>')
 utils.nmap("<nop>", "<Plug>Markdown_Fold")               -- tab is for moving around only
-utils.lnmap("nh", "<Plug>Markdown_MoveToNextHeader")     -- tab is for moving around only
-utils.lnmap("ph", "<Plug>Markdown_MoveToPreviousHeader") -- tab is for moving around only
+utils.lnmap("]]", "<Plug>Markdown_MoveToNextHeader")     -- tab is for moving around only
+utils.lnmap("[[", "<Plug>Markdown_MoveToPreviousHeader") -- tab is for moving around only
 utils.lnmap("ctd", "4wvg$y")                             -- copy description from the taskwarrior task in the markdown format
 -- cut and copy content to next header #
 utils.nmap("cO", ":.,/^#/-1d<cr>")
