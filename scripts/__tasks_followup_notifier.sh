238b2cba06d92c848b3f8e0fc6ce0f061f860942
Piotr <decoder@live.de>
2024-03-15 11:52:49 +0100

    taskwarrior: simple cron reminder to follow up on any outstanding issues
    Signed-off-by: Piotr <decoder@live.de>

---
+++ scripts/__tasks_followup_notifier.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export DISPLAY=:1
+
+# Get the list of task IDs where follow is set to Y
+tasks=$(task follow.is:Y _ids)
+
+# Check if the tasks list is not empty
+if [ -n "$tasks" ]; then
+	# Send the notification
+	zenity --info --text "You have follow-up tasks to complete. Please check the task list."
+fi
