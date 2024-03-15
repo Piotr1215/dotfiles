8a5631518c90b67ede5d9b03720e285eb67e70f8
Piotr <decoder@live.de>
2024-03-15 11:51:16 +0100

    autokey: append to playlist now doesn't need selector
    Signed-off-by: Piotr <decoder@live.de>

--- scripts/__append_to_playlist.py
+++ scripts/__append_to_playlist.py
@@ -1,46 +1,35 @@
 #!/usr/bin/env python3
-# This script is used to append a URL to a playlist file.
-# Part of PROJECT:playlist
-import json
 import sys
-import struct
 import logging
+import os
 
-logging.basicConfig(filename='/home/decoder/logme.log', level=logging.DEBUG)
+logging.basicConfig(filename=os.path.expanduser('/home/decoder/logme.log'), level=logging.DEBUG)
 
-def getMessage():
-    rawLength = sys.stdin.buffer.read(4)
-    if len(rawLength) == 0:
-        sys.exit(0)
-    messageLength = struct.unpack('@I', rawLength)[0]
-    message = sys.stdin.buffer.read(messageLength).decode('utf-8')
-    return json.loads(message)
-
-def append_to_playlist(url, playlist_file_path):
-    logging.debug(f'Appending URL: {url} to playlist: {playlist_file_path}')
+def append_to_playlist(url, description, playlist_file_path):
+    logging.debug(f'Appending URL: {url} with description: {description} to playlist: {playlist_file_path}')
+    entry = f"# {description}\n{url}\n"
     with open(playlist_file_path, 'r') as f:
-        existing_urls = f.readlines()
+        existing_content = f.read()
 
-    existing_urls = [line.strip() for line in existing_urls]
-
-    if url not in existing_urls:
+    if url not in existing_content:
         with open(playlist_file_path, 'a') as f:
-            f.write(url + '\n')
-        logging.debug(f'URL: {url} appended successfully.')
+            f.write(entry)
+        logging.debug('Entry appended successfully.')
     else:
-        logging.debug(f'URL: {url} already exists in playlist.')
+        logging.debug('URL already exists in playlist.')
 
 def main():
-    logging.debug('Script started')
-    receivedMessage = getMessage()
-    url = receivedMessage.get("url")
-    
-    if url:
-        playlist_file_path = "/home/decoder/haruna_playlist.m3u"
-        append_to_playlist(url, playlist_file_path)
-        logging.debug('Success response sent.')
-    else:
-        logging.debug('Error response sent due to missing URL.')
+    if len(sys.argv) < 4:
+        logging.error('Usage: __append_to_playlist.py <url> <description> <playlist_file_path>')
+        sys.exit(1)
+
+    url = sys.argv[1]
+    description = sys.argv[2]
+    playlist_file_path = os.path.expanduser(sys.argv[3])
+
+    append_to_playlist(url, description, playlist_file_path)
+    logging.debug('Success response sent.')
 
 if __name__ == "__main__":
     main()
+
