181a57705d4b6bd983dc6f1dc3cf19fe2be5e048
Piotr <decoder@live.de>
2024-03-29 20:30:50 +0100

    nvim: nvimrc config doens't need lua extension to follow the same pattern as other configs
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,11 +10,11 @@ require("user_functions")
 require("projects")
 
 -- PROJECT: project-config
--- Searches for a .nvimrc.lua file from the current directory up to the root and executes it if found.
+-- Searches for a .nvimrc file from the current directory up to the root and executes it if found.
 vim.api.nvim_create_autocmd("BufEnter", {
     pattern = "*",
     callback = function()
-      local project_config_path = vim.fn.findfile(".nvimrc.lua", ".;")
+      local project_config_path = vim.fn.findfile(".nvimrc", ".;")
       if project_config_path ~= "" then
           loadfile(project_config_path)()
       end
