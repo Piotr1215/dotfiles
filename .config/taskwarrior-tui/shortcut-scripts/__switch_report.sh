f2e2b57f0f9db28c9ce2b8490f62d309a1c0d36a
Piotr <decoder@live.de>
2024-04-01 11:24:03 +0200

    task: taskwarrior-tui context
    Contexts are now easier to switch using 9 binding and do not spawn
    additional shell. Taskwarrior-tui starts now by switching to work
    context every time it starts.
    
    Signed-off-by: Piotr <decoder@live.de>

--- .config/taskwarrior-tui/shortcut-scripts/__switch_report.sh
+++ .config/taskwarrior-tui/shortcut-scripts/__switch_report.sh
@@ -1,2 +1,25 @@
 #!/usr/bin/env bash
-tui --report="$1"
+
+marker_file="$HOME/.task_context_switch"
+
+switch_context() {
+	task context "$1"
+	echo "Switched to $1 context."
+}
+
+# Get the current task context
+current_context=$(task _get rc.context)
+
+# Check if the current context is "work"
+if [ "$current_context" == "work" ]; then
+	switch_context home
+	# Set the marker file to indicate we should switch back to work next time
+	echo "work" >"$marker_file"
+elif [ "$current_context" == "home" ] && [ -f "$marker_file" ]; then
+	# If we're in home and the marker file exists, it means we want to switch back to work
+	switch_context work
+	# Remove the marker file after switching back to avoid repeated switches
+	rm "$marker_file"
+else
+	echo "Current context is: $current_context. No action taken."
+fi
