90c682923aea614bd4109404ea779685db1e023b
Piotr <decoder@live.de>
2024-04-20 21:04:32 +0200

    nvim: scripts formatting
    Signed-off-by: Piotr <decoder@live.de>

--- scripts/__open-file.sh
+++ scripts/__open-file.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Source generic error handling function
-source __trap.sh
-
 # The set -e option instructs bash to immediately exit if any command has a non-zero exit status
 # The set -u referencing a previously undefined variable - with the exceptions of $* and $@ - is an error
 # The set -o pipefaile if any command in a pipeline fails, that return code will be used as the return code of the whole pipeline
@@ -47,8 +44,6 @@ handle_fzf_error() {
 		return $?
 	fi
 }
-
-# Add source and line number when running in debug mode: __run_with_xtrace.sh __open-file.sh
 
 # Set new line and tab for word splitting
 IFS=$'\n' files=($(fzf-tmux --preview "bat --color=always {$1}" --reverse --query="$1" --multi --select-1 --exit-0 || handle_fzf_error))
