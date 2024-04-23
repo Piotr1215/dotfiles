698e438336aae270114d9f11a242da530da86f02
Piotr <decoder@live.de>
2024-04-23 20:57:08 +0200

    nvim: upgrade chatgpt API to newest and move configuraiton to a separate file
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -15,43 +15,7 @@ return require("packer").startup(function(use)
   use "wbthomason/packer.nvim"
   -- AI {{{
   use "github/copilot.vim"
-  use {
-    "robitx/gp.nvim",
-    config = function()
-      require("gp").setup {
-        agents = {
-          -- Disable ChatGPT 3.5
-          {
-            name = "ChatGPT3-5",
-            chat = false, -- just name would suffice
-            command = false, -- just name would suffice
-          },
-          {
-            name = "ChatGPT4",
-            chat = true,
-            command = true,
-            -- string with model name or table with model name and parameters
-            model = { model = "gpt-4-1106-preview", temperature = 1.1, top_p = 1 },
-            -- system prompt (use this to specify the persona/role of the AI)
-            system_prompt = "You are a general AI assistant.\n\n"
-              .. "The user provided the additional info about how they would like you to respond:\n\n"
-              .. "- If you're unsure don't guess and say you don't know instead.\n"
-              .. "- Ask question if you need clarification to provide better answer.\n"
-              .. "- Think deeply and carefully from first principles step by step.\n"
-              .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-              .. "- Use Socratic method to improve your thinking and coding skills.\n"
-              .. "- Don't elide any code from your output if the answer requires coding.\n"
-              .. "- Take a deep breath; You've got this!\n",
-          },
-        },
-      }
-
-      -- or setup with your own config (see Install > Configuration in Readme)
-      -- require("gp").setup(conf)
-
-      -- shortcuts might be setup here (see Usage > Shortcuts in Readme)
-    end,
-  }
+  use { "robitx/gp.nvim" }
   use { "MunifTanjim/nui.nvim" }
   use {
     "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
--- .config/nvim/lua/settings/plugins-setup.lua
+++ .config/nvim/lua/settings/plugins-setup.lua
@@ -7,6 +7,35 @@ require("yanksearch").setup {
   lines_below = 0,
   lines_around = 0, -- This will override lines_above and lines_below if set to a non-zero value
 }
+require("gp").setup {
+  require("gp").setup {
+    agents = {
+      -- Disable ChatGPT 3.5
+      {
+        name = "ChatGPT3-5",
+        chat = false, -- just name would suffice
+        command = false, -- just name would suffice
+      },
+      {
+        name = "ChatGPT4",
+        chat = true,
+        command = true,
+        -- string with model name or table with model name and parameters
+        model = { model = "gpt-4-turbo-preview", temperature = 0, top_p = 1 },
+        -- system prompt (use this to specify the persona/role of the AI)
+        system_prompt = "You are a general AI assistant.\n\n"
+          .. "The user provided the additional info about how they would like you to respond:\n\n"
+          .. "- If you're unsure don't guess and say you don't know instead.\n"
+          .. "- Ask question if you need clarification to provide better answer.\n"
+          .. "- Think deeply and carefully from first principles step by step.\n"
+          .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+          .. "- Use Socratic method to improve your thinking and coding skills.\n"
+          .. "- Don't elide any code from your output if the answer requires coding.\n"
+          .. "- Take a deep breath; You've got this!\n",
+      },
+    },
+  },
+}
 require("codesnap").setup {
   -- ...
   watermark = "󰭪",
