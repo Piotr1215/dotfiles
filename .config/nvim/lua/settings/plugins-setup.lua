2c3a9cf12c868173daa4f5b3fdb6cd284a519b75
Piotr <decoder@live.de>
2024-04-25 10:02:35 +0200

    proj: project tags
    Upgrading PROJECT tags handling:
    - source projects in `nvim-cmp` from a file
    - dynamically add projects from neovim to the file
    - alias for editing the projects file
    - helper function to list all files belonging to a project
    - compdef for the project list function
    
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/settings/plugins-setup.lua
+++ .config/nvim/lua/settings/plugins-setup.lua
@@ -80,6 +80,15 @@ require("mdeval").setup {
       exec_type = "interpreted", -- Since Go runs as 'go run' for scripts
       language_code = "go", -- Assuming the plugin can use this to identify code blocks
     },
+    zsh = {
+      command = { "zsh", "-c" },
+      exec_type = "interpreted",
+      language_code = "zsh",
+      extension = "sh",
+      default_header = [[
+#!/usr/bin/env zsh
+      ]],
+    },
     cpp = {
       command = { "clang++", "-std=c++20", "-O0" },
       default_header = [[
