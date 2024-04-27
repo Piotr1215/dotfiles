1b81e510a6c8c5192e0bc821d1427b5ec4a4b46f
Piotr <decoder@live.de>
2024-04-27 09:59:21 +0200

    nvim: tick time from date too
    Signed-off-by: Piotr <decoder@live.de>

--- scripts/__ticker.sh
+++ scripts/__ticker.sh
@@ -2,13 +2,15 @@
 
 TARGET_DATE="2024-05-31"
 
-CURRENT_DATE=$(date +%Y-%m-%d)
-
-DAYS_LEFT=$(($(date -d "$TARGET_DATE" +%s) - $(date -d "$CURRENT_DATE" +%s)))
+DAYS_LEFT=$(($(date -d "$TARGET_DATE" +%s) - $(date +%s)))
 DAYS_LEFT=$(($DAYS_LEFT / 86400)) # Convert seconds to days
+
+# Calculate days passed since 15th April of the current year
+DAYS_FROM=$(($(date +%s) - $(date -d "$(date +%Y)-04-15" +%s)))
+DAYS_FROM=$(($DAYS_FROM / 86400)) # Convert seconds to days
 
 if [ $DAYS_LEFT -lt 0 ]; then
 	echo "The date has passed!"
 else
-	echo $DAYS_LEFT days | figlet -f term | boxes -d peek -a c
+	echo -e "$DAYS_LEFT days left\n$DAYS_FROM passed" | figlet -f term | boxes -d peek -a c
 fi
