42b83efa2d620db78c2908ecc9cf2199fdac5a3d
Piotr <decoder@live.de>
2024-04-28 16:44:46 +0200

    nvim: before.nvim with default bindings, move back and forth between edits
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -64,6 +64,22 @@ return require("packer").startup(function(use)
       "nvim-telescope/telescope.nvim", -- optional: for multi-select
     },
   }
+  use {
+    "bloznelis/before.nvim",
+    config = function()
+      local before = require "before"
+      before.setup()
+
+      -- Jump to previous entry in the edit history
+      vim.keymap.set("n", "<C-h>", before.jump_to_last_edit, {})
+
+      -- Jump to next entry in the edit history
+      vim.keymap.set("n", "<C-l>", before.jump_to_next_edit, {})
+
+      -- Look for previous edits in telescope (needs telescope, obviously)
+      vim.keymap.set("n", "<leader>oe", before.show_edits_in_telescope, {})
+    end,
+  }
   use { "ionide/Ionide-vim" }
   use "rcarriga/nvim-notify"
   use { "marcelofern/vale.nvim" }
