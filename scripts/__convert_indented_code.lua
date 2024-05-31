774dc71f1630faa44440019c11336117f95b2586
Piotr <decoder@live.de>
2024-05-31 09:16:24 +0200

    pandoc: this is useul when working with medium markdown to converd indented code blocks to fenced
    Signed-off-by: Piotr <decoder@live.de>

---
+++ scripts/__convert_indented_code.lua
@@ -0,0 +1,8 @@
+local fenced = "```\n%s\n```\n"
+function CodeBlock(cb)
+	-- use pandoc's default behavior if the block has classes or attribs
+	if cb.classes[1] or next(cb.attributes) then
+		return nil
+	end
+	return pandoc.RawBlock("bash", cb.text)
+end
