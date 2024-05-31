11164bd3bda2e54a958a319dca027f3bf139c129
Piotr <decoder@live.de>
2024-05-31 12:38:27 +0200

    nvim: fixing trouble mappings after breaking change
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -209,8 +209,8 @@ utils.lnmap("tt", ":FloatermToggle<CR>")
 utils.tmap("<leader>tt", "<C-\\><C-n>:FloatermToggle<CR>")
 
 -- Trouble
-vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
