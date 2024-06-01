d591615d011bd0a12842d8528e0b4908e99f9794
Piotr <decoder@live.de>
2024-06-01 13:11:25 +0200

    nvim: obsidianlink just needs enter to work with names
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -221,7 +221,7 @@ utils.lnmap("f2", "<cmd>FIX 25<cr>")
 utils.lnmap("f0", "<cmd>FIX -1<cr>")
 
 -- Obsidian
-utils.vmap("ol", ":ObsidianLink<leader>", { silent = false })
+utils.vmap("ol", ":ObsidianLink<cr>", { silent = false })
 utils.lnmap("oq", ":ObsidianQuickSwitch<cr>")
 utils.lnmap("on", ":ObsidianNew ", { silent = false })
 utils.vmap("on", ":ObsidianLinkNew ", { silent = false })
