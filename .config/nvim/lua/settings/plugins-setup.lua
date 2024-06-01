a6aa0c946ec96909ee6de6a36b2ff707a1ad60b5
Piotr <decoder@live.de>
2024-06-01 13:09:20 +0200

    nvim: gitsigns change default signs
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/settings/plugins-setup.lua
+++ .config/nvim/lua/settings/plugins-setup.lua
@@ -146,6 +146,14 @@ require("hurl").setup {
 }
 
 require("gitsigns").setup {
+  signs = {
+    add = { text = "➕" },
+    change = { text = "󱎝" },
+    delete = { text = "➖" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "" },
+    untracked = { text = "┆" },
+  },
   on_attach = function(bufnr)
     local gs = package.loaded.gitsigns
 
