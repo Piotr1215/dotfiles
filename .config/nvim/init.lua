e5cd10ae2e8447d05d99083cfdf024046606b956
Piotr <decoder@live.de>
2024-06-27 16:16:54 +0200

    nvim: typing simulation script
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,22 +1,23 @@
-require("plugins")
-require("settings")
-require("autocommands")
-require("mappings")
-require("telescope-setup")
-require("lsp")
+require "plugins"
+require "settings"
+require "autocommands"
+require "mappings"
+require "telescope-setup"
+require "lsp"
 -- require("lsp-setup")
-require("which-key-setup")
-require("user_functions")
-require("projects")
+require "which-key-setup"
+require "user_functions"
+require "projects"
+require "typing_simulation"
 
 -- PROJECT: project-config
 -- Searches for a .nvimrc file from the current directory up to the root and executes it if found.
 vim.api.nvim_create_autocmd("BufEnter", {
-    pattern = "*",
-    callback = function()
-      local project_config_path = vim.fn.findfile(".nvimrc", ".;")
-      if project_config_path ~= "" then
-          loadfile(project_config_path)()
-      end
+  pattern = "*",
+  callback = function()
+    local project_config_path = vim.fn.findfile(".nvimrc", ".;")
+    if project_config_path ~= "" then
+      loadfile(project_config_path)()
     end
+  end,
 })
