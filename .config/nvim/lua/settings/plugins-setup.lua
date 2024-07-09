df92c3f1187090ad7501ce14ce224bec821d9cc5
Piotr <decoder@live.de>
2024-07-09 22:23:52 +0200

    nvim: remove dot repeat from treesitter, doesn't work after update
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/settings/plugins-setup.lua
+++ .config/nvim/lua/settings/plugins-setup.lua
@@ -413,14 +413,6 @@ require("nvim-treesitter.configs").setup {
   },
 }
 
-local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
-
--- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
-
 require("femaco").setup {
   -- what to do after opening the float
   post_open_float = function(winnr)
