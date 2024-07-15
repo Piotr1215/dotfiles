4c072d583273b9b6f26dbd5559a100837eeed243
Piotr <decoder@live.de>
2024-07-15 09:37:07 +0200

    nvim: fix which-key setup after recent updates
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/which-key-setup.lua
+++ .config/nvim/lua/which-key-setup.lua
@@ -1,21 +1,9 @@
-local wk = require("which-key")
--- As an example, we will create the following mappings:
---  * <leader>ff find files
---  * <leader>fr show recent files
---  * <leader>fb Foobar
--- we'll document:
---  * <leader>fn new file
---  * <leader>fe edit file
--- and hide <leader>1
+local wk = require "which-key"
 
 wk.register({
-  f = {
-    name = "file", -- optional group name
-    f = { "Find File" }, -- create a binding with label
-    r = { "Open Recent File", noremap=false }, -- additional options for creating the keymap
-    n = { "New File" }, -- just a label. don't create any mapping
-    e = "Edit File", -- same as above
-    ["1"] = "which_key_ignore",  -- special label to hide it in the popup
-    b = { function() print("bar") end, "Foobar" } -- you can also pass functions!
-  },
+  ["<leader>f"] = { name = "file" },
+  ["<leader>fe"] = { "Edit File" },
+  ["<leader>ff"] = { "Find File" },
+  ["<leader>fn"] = { "New File" },
+  ["<leader>fr"] = { "Open Recent File", remap = true },
 }, { prefix = "<leader>" })
