19343e10d18e87182d36a801fb86e2f2a29f9f92
Piotr <decoder@live.de>
2024-08-04 22:44:22 +0200

    nvim: improving cmp
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/lsp/cmp.lua
+++ .config/nvim/lua/lsp/cmp.lua
@@ -1,45 +1,17 @@
 -- nvim-cmp
-local lspkind = require('lspkind')
-local cmp = require("cmp")
+local lspkind = require "lspkind"
+local cmp = require "cmp"
 
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
 
 local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
-local cmp_kinds = {
-  Function = "",
-  Text = "",
-  Method = "",
-  Constructor = "",
-  Field = "ﰠ",
-  Variable = "",
-  Class = "ﴯ",
-  Interface = "",
-  Module = "",
-  Property = "ﰠ",
-  Unit = "塞",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "פּ",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
-}
-
-cmp.setup({
+cmp.setup {
   snippet = {
     expand = function(args)
       vim.fn["vsnip#anonymous"](args.body)
@@ -47,34 +19,36 @@ cmp.setup({
   },
 
   formatting = {
-    format = lspkind.cmp_format({
-      preset = 'codicons',
-      symbol_map = cmp_kinds, -- The glyphs will be used by `lspkind`
+    expandable_indicator = true,
+    fields = {
+      "abbr",
+      "kind",
+    },
+    format = lspkind.cmp_format {
+      preset = "codicons",
       async = true,
-      menu = ({
+      menu = {
         buffer = "[Buffer]",
         nvim_lsp = "[LSP]",
-        luasnip = "[LuaSnip]",
         nvim_lua = "[Lua]",
-        latex_symbols = "[Latex]",
         projects = "[Projects]",
-      }),
-    }),
+        emoji = "[Emoji]",
+      },
+    },
   },
 
   mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
 
-    -- Use Ctrl + j and Shift-Ctrl + j to browse through the suggestions.
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
@@ -97,31 +71,31 @@ cmp.setup({
   },
 
   sources = {
-    { name = 'nvim_lsp' },
-    { name = 'nvim_lua', priority = 100 },
-    { name = 'vsnip' },
-    { name = 'buffer' },
-    { name = 'emoji' },
-    { name = 'path' },
+    { name = "nvim_lsp" },
+    { name = "nvim_lua", priority = 100 },
+    { name = "vsnip" },
+    { name = "buffer" },
+    { name = "emoji" },
+    { name = "path" },
     { name = "crates" },
     { name = "projects", priority = 100 },
   },
-})
+}
 
 -- Use buffer source for `/`
-cmp.setup.cmdline('/', {
+cmp.setup.cmdline("/", {
   sources = {
-    { name = 'buffer' }
-  }
+    { name = "buffer" },
+  },
 })
 
 -- Use cmdline & path source for ':'
-cmp.setup.cmdline(':', {
+cmp.setup.cmdline(":", {
   sources = cmp.config.sources({
-    { name = 'path' }
+    { name = "path" },
   }, {
-    { name = 'cmdline' }
-  })
+    { name = "cmdline" },
+  }),
 })
 
 local highlight = {
