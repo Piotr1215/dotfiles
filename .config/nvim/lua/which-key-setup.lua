6238b38e3b9033a7746b3dae1b65c87c5a487f23
Piotr <decoder@live.de>
2024-08-10 22:53:22 +0200

    nvim: remove redundant which-key setup
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/which-key-setup.lua
+++
@@ -1,9 +0,0 @@
-local wk = require "which-key"
-
-wk.register({
-  ["<leader>f"] = { name = "file" },
-  ["<leader>fe"] = { "Edit File" },
-  ["<leader>ff"] = { "Find File" },
-  ["<leader>fn"] = { "New File" },
-  ["<leader>fr"] = { "Open Recent File", remap = true },
-}, { prefix = "<leader>" })
