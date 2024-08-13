d08ffc2e663563299462fb7f1cc8744477b23199
Piotr <decoder@live.de>
2024-08-13 10:23:26 +0200

    nvim: update gp.nvim settings and add custom context instructions to the dotfiles repo
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/settings/plugins-setup.lua
+++ .config/nvim/lua/settings/plugins-setup.lua
@@ -56,15 +56,15 @@ require("gp").setup {
         -- string with model name or table with model name and parameters
         model = { model = "gpt-4o", temperature = 0.1, top_p = 1 },
         -- system prompt (use this to specify the persona/role of the AI)
-        system_prompt = "You are a general AI assistant.\n\n"
+        system_prompt = "You are a specialized coding AI assistant.\n\n"
           .. "The user provided the additional info about how they would like you to respond:\n\n"
           .. "- If you're unsure don't guess and say you don't know instead.\n"
           .. "- Ask question if you need clarification to provide better answer.\n"
           .. "- Think deeply and carefully from first principles step by step.\n"
-          .. "- Zoom out first to see the big picture and then zoom in to details.\n"
-          .. "- Use Socratic method to improve your thinking and coding skills.\n"
-          .. "- Don't elide any code from your output if the answer requires coding.\n"
-          .. "- Take a deep breath; You've got this!\n",
+          .. "- Make your answers short, conscience, to the point and helpful.\n"
+          .. "- Produce only valid and actionable code.\n"
+          .. "- Include only essencial response like code etc, DO NOT provide explanations unless specifically asked for\n"
+          .. "- Take a deep breath; You've got this!",
       },
     },
   },
