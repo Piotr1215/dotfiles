5caf84991b6eda2f037c018a976e93eecf2d31ad
Piotr <decoder@live.de>
2024-08-15 22:56:32 +0200

    nvim: swap default flash.nvim treesitter mode to T as it collides with surround
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -48,12 +48,13 @@ return require("lazy").setup {
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "T", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
   },
+  "tyru/open-browser.vim",
   "towolf/vim-helm",
   "stefandtw/quickfix-reflector.vim",
   "jbyuki/one-small-step-for-vimkind",
