82e46de33173d1509a30be2d2398ba70c0285f9d
Piotr <decoder@live.de>
2024-09-02 12:14:48 +0200

    autokey: add more slack windows combos
    Signed-off-by: Piotr <decoder@live.de>

--- .config/autokey/data/Scripts/L_slack_firefox.py
+++ .config/autokey/data/Scripts/L_slack_firefox.py
@@ -4,4 +4,4 @@ import subprocess
 # Alacritty in 9:16 mode for shorts recording
 layout = '/home/decoder/dev/dotfiles/scripts/__layouts.sh'
 
-subprocess.run([layout, "10"])
+subprocess.run([layout, "4"])
