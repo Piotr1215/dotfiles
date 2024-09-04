35cd3449234daf8ab21cfb41d45b32cbcb226d0a
Piotr <decoder@live.de>
2024-09-04 22:16:29 +0200

    autokey: fix issue insert to work well with linear
    Signed-off-by: Piotr <decoder@live.de>

--- .config/autokey/data/Scripts/Insert IT.py
+++ .config/autokey/data/Scripts/Insert IT.py
@@ -1,20 +1,18 @@
 markdown_template = '''## Description
 
-<!-- - tasks description
-     - steps to solve
-     - background and additional context
-     - optionally As a [user], I want [functionality], so that I can [benefit].
--->
+- issue description
+- background and additional context
+- optionally As a [user], I want [functionality], so that I can [benefit].
+
 
 ## Acceptance Criteria
 
 - [ ]
 
-<!-- how can someone test it and tell that the task is done -->
 
 ## Related
 
-<!-- patent issues, related and blockers -->
+- patent issues, related and blockers
 '''
 
 # Set the clipboard content using AutoKey's built-in functionality
