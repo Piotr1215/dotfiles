4f51cdaa9b1a76800734b2b535f1e4e27a1f663b
Piotr <decoder@live.de>
2024-09-08 14:18:38 +0200

    scripts: sort and pretty output for the youtube pull script
    Signed-off-by: Piotr <decoder@live.de>

--- scripts/__get_youtube_videos.py
+++ scripts/__get_youtube_videos.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import googleapiclient.discovery
 import os
+import argparse
+from prettytable import PrettyTable
 
 # Replace with your API key and channel ID from environment variables
 api_key = os.getenv('YOUTUBE_API_KEY')
@@ -9,7 +11,7 @@ channel_id = os.getenv('YOUTUBE_CHANNEL_ID')
 # Initialize YouTube API
 youtube = googleapiclient.discovery.build('youtube', 'v3', developerKey=api_key)
 
-def get_all_video_titles_and_shorts(channel_id):
+def get_all_video_data(channel_id):
     videos = []
     shorts = []
     next_page_token = None
@@ -33,7 +35,7 @@ def get_all_video_titles_and_shorts(channel_id):
 
         # Fetch detailed information about each video
         video_request = youtube.videos().list(
-            part="snippet,contentDetails",
+            part="snippet,contentDetails,statistics",
             id=",".join(video_ids)
         )
         video_response = video_request.execute()
@@ -41,12 +43,22 @@ def get_all_video_titles_and_shorts(channel_id):
         for video in video_response['items']:
             title = video['snippet']['title']
             duration = video['contentDetails']['duration']
+            views = int(video['statistics'].get('viewCount', '0'))  # Convert views to int
+            likes = int(video['statistics'].get('likeCount', '0'))  # Convert likes to int
 
             # Shorts are under 60 seconds (PT1M or less)
             if 'PT1M' in duration or ('PT' in duration and 'M' not in duration):  # Shorts check
-                shorts.append(title)
+                shorts.append({
+                    "title": title,
+                    "views": views,
+                    "likes": likes
+                })
             else:
-                videos.append(title)
+                videos.append({
+                    "title": title,
+                    "views": views,
+                    "likes": likes
+                })
 
         # Check if there's another page of results
         next_page_token = response.get('nextPageToken')
@@ -56,17 +68,46 @@ def get_all_video_titles_and_shorts(channel_id):
 
     return videos, shorts
 
-# Get videos and shorts separately
-videos, shorts = get_all_video_titles_and_shorts(channel_id)
+def display_table(videos, shorts, show_shorts_stats):
+    # Sort videos by views
+    videos = sorted(videos, key=lambda x: x['views'], reverse=True)
 
-# Print videos with count numbers
-print("Videos:")
-for i, video in enumerate(videos, 1):
-    print(f"{i}. {video}")
+    # Print videos with headers using PrettyTable
+    video_table = PrettyTable()
+    video_table.field_names = ["#", "Title", "Views", "Likes"]
+    video_table.align["Title"] = "l"  # Left align the title
 
-print("\nShorts:")
-for i, short in enumerate(shorts, 1):
-    print(f"{i}. {short}")
+    for i, video in enumerate(videos, 1):
+        video_table.add_row([i, video['title'], video['views'], video['likes']])
+
+    print("Videos:")
+    print(video_table)
+
+    # Only print shorts if the --shorts flag is passed
+    if show_shorts_stats:
+        shorts = sorted(shorts, key=lambda x: x['views'], reverse=True)
+        short_table = PrettyTable()
+        short_table.field_names = ["#", "Title", "Views", "Likes"]
+        short_table.align["Title"] = "l"  # Left align the title
+
+        for i, short in enumerate(shorts, 1):
+            short_table.add_row([i, short['title'], short['views'], short['likes']])
+
+        print("\nShorts:")
+        print(short_table)
+    elif len(shorts) > 0:  # If the flag is not passed, show no shorts
+        print("\nShorts are hidden (use --shorts flag to display them)")
+
+# Argument parser to add --shorts flag
+parser = argparse.ArgumentParser(description="Fetch YouTube video data")
+parser.add_argument('--shorts', action='store_true', help='Include shorts statistics in the output')
+args = parser.parse_args()
+
+# Get videos and shorts with views and likes
+videos, shorts = get_all_video_data(channel_id)
+
+# Display the tables
+display_table(videos, shorts, args.shorts)
 
 # Summarize the results
 total_videos = len(videos)
