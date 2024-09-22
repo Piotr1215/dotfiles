de2c78892b948147d3af17e04a73f6d97decb723
Piotr <decoder@live.de>
2024-09-22 14:48:19 +0200

    nvim: remove avante in favor of gp.nvim
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -20,53 +20,8 @@ return require("lazy").setup {
   "github/copilot.vim",
   "robitx/gp.nvim",
   "MunifTanjim/nui.nvim",
-  {
-    "jellydn/hurl.nvim",
-  },
-  {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      -- add any opts here
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
+  "jellydn/hurl.nvim",
+  "stevearc/dressing.nvim",
   -- }}}
   -- Editor Extensions {{{
   {
@@ -82,6 +37,8 @@ return require("lazy").setup {
   },
   "tyru/open-browser.vim",
   "gcmt/taboo.vim",
+  "MeanderingProgrammer/render-markdown.nvim",
+
   "towolf/vim-helm",
   "jbyuki/one-small-step-for-vimkind",
   {
