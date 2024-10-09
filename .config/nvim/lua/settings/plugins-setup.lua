03e7b276a951adab7f3fdc5d3a9f53bbda64535e
Piotr <decoder@live.de>
2024-10-09 16:37:00 +0200

    nvim: bring back gitsigns
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/settings/plugins-setup.lua
+++ .config/nvim/lua/settings/plugins-setup.lua
@@ -9,6 +9,61 @@ require("typeit").setup {
 
 require("Comment").setup()
 
+require("gitsigns").setup {
+  on_attach = function(bufnr)
+    local gitsigns = require "gitsigns"
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map("n", "]c", function()
+      if vim.wo.diff then
+        vim.cmd.normal { "]c", bang = true }
+      else
+        gitsigns.nav_hunk "next"
+      end
+    end, { desc = "Go to next hunk" })
+
+    map("n", "[c", function()
+      if vim.wo.diff then
+        vim.cmd.normal { "[c", bang = true }
+      else
+        gitsigns.nav_hunk "prev"
+      end
+    end, { desc = "Go to previous hunk" })
+
+    -- Actions
+    map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage hunk" })
+    map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "Reset hunk" })
+    map("v", "<leader>hs", function()
+      gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
+    end, { desc = "Stage hunk (visual)" })
+    map("v", "<leader>hr", function()
+      gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
+    end, { desc = "Reset hunk (visual)" })
+    map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "Stage buffer" })
+    map("n", "<leader>hu", gitsigns.undo_stage_hunk, { desc = "Undo stage hunk" })
+    map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Reset buffer" })
+    map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+    map("n", "<leader>hb", function()
+      gitsigns.blame_line { full = true }
+    end, { desc = "Blame line" })
+    map("n", "<leader>tb", gitsigns.toggle_current_line_blame, { desc = "Toggle line blame" })
+    map("n", "<leader>hd", gitsigns.diffthis, { desc = "Diff this" })
+    map("n", "<leader>hD", function()
+      gitsigns.diffthis "~"
+    end, { desc = "Diff this (against HEAD)" })
+    map("n", "<leader>td", gitsigns.toggle_deleted, { desc = "Toggle deleted" })
+
+    -- Text object
+    map({ "o", "x" }, "Ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "Select hunk" })
+  end,
+}
+
 require("yanksearch").setup {
   lines_above = 0,
   lines_below = 0,
