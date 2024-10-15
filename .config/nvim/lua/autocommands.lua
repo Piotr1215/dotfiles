3f526e9f1a47a4f149a6cc3c34cf59267cc7345b
Piotr <decoder@live.de>
2024-10-15 10:32:55 +0200

    nvim: add simple commandto lowecase first letters in selection
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -54,6 +54,13 @@ local function generate_plantuml()
 end
 
 -- User Commands
+
+vim.api.nvim_create_user_command("LowercaseFirstLetter", function(opts)
+  local line1, line2 = opts.line1, opts.line2
+  vim.cmd(string.format("%d,%ds/\\%%V\\<./\\l&/g", line1, line2))
+end, { range = true })
+vim.cmd "cnoreabbrev lc LowercaseFirstLetter"
+
 vim.api.nvim_create_user_command("ClearQF", function()
   vim.fn.setqflist {}
 end, {})
