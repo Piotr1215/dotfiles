8ab7b46694acf7c457f8482f8cef8b998b7796cc
Piotr <decoder@live.de>
2024-10-20 17:00:02 +0200

    yazi: downloads keymap
    Signed-off-by: Piotr <decoder@live.de>

--- scripts/__trigger_yazi.sh
+++ scripts/__trigger_yazi.sh
@@ -1,7 +1,18 @@
-#!/bin/bash
-# This script is part used in the tmux.conf file to trigger yazi
-if [[ $(tmux display-message -p '#{pane_current_command}') == "nvim" ]]; then
+#!/usr/bin/env bash
+
+y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	EDITOR=nvim yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		tmux send-keys "cd $cwd" C-m
+	fi
+	rm -f -- "$tmp"
+}
+
+current_command=$(tmux display-message -p '#{pane_current_command}')
+
+if [[ "$current_command" == "nvim" ]]; then
 	tmux send-keys 'Space' '-'
 else
-	tmux popup -d '#{pane_current_path}' -E -h 95% -w 95% -x 100% 'yazi'
+	tmux popup -d '#{pane_current_path}' -E -h 95% -w 95% -x 100% "bash -c 'source $0; y'"
 fi
