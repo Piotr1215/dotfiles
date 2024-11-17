781a22a5564e4759df429178770e93c8b0aa1e8f
Piotr <decoder@live.de>
2024-11-17 09:41:57 +0100

    nvim: ruff_lsp is deprecated, switching to ruff
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/lsp/python-setup.lua
+++ .config/nvim/lua/lsp/python-setup.lua
@@ -4,46 +4,46 @@ require("nvim-dap-virtual-text").setup()
 local def = require "lsp.default-lsp"
 
 local lspconfig = require "lspconfig"
-local util = require 'lspconfig.util'
+local util = require "lspconfig.util"
 
 local root_files = {
-  'pyproject.toml',
-  'setup.py',
-  'setup.cfg',
-  'requirements.txt',
-  'Pipfile',
-  'pyrightconfig.json',
-  '.git',
+  "pyproject.toml",
+  "setup.py",
+  "setup.cfg",
+  "requirements.txt",
+  "Pipfile",
+  "pyrightconfig.json",
+  ".git",
 }
 local function organize_imports()
   local params = {
-    command = 'pyright.organizeimports',
+    command = "pyright.organizeimports",
     arguments = { vim.uri_from_bufnr(0) },
   }
 
   local clients = vim.lsp.get_active_clients {
     bufnr = vim.api.nvim_get_current_buf(),
-    name = 'pyright',
+    name = "pyright",
   }
   for _, client in ipairs(clients) do
-    client.request('workspace/executeCommand', params, nil, 0)
+    client.request("workspace/executeCommand", params, nil, 0)
   end
 end
 local function set_python_path(path)
   local clients = vim.lsp.get_active_clients {
     bufnr = vim.api.nvim_get_current_buf(),
-    name = 'pyright',
+    name = "pyright",
   }
   for _, client in ipairs(clients) do
-    client.config.settings = vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = path } })
-    client.notify('workspace/didChangeConfiguration', { settings = nil })
+    client.config.settings = vim.tbl_deep_extend("force", client.config.settings, { python = { pythonPath = path } })
+    client.notify("workspace/didChangeConfiguration", { settings = nil })
   end
 end
-lspconfig.ruff_lsp.setup {
+lspconfig.ruff.setup {
   capabilities = def.capabilities,
   on_attach = def.on_attach,
   default_config = {
-    filetypes = { 'python' },
+    filetypes = { "python" },
     single_file_support = true,
   },
 }
@@ -51,8 +51,8 @@ lspconfig.pyright.setup {
   capabilities = def.capabilities,
   on_attach = def.on_attach,
   default_config = {
-    cmd = { 'pyright-langserver', '--stdio' },
-    filetypes = { 'python' },
+    cmd = { "pyright-langserver", "--stdio" },
+    filetypes = { "python" },
     root_dir = function(fname)
       return util.root_pattern(unpack(root_files))(fname)
     end,
@@ -62,7 +62,7 @@ lspconfig.pyright.setup {
         analysis = {
           autoSearchPaths = true,
           useLibraryCodeForTypes = true,
-          diagnosticMode = 'openFilesOnly',
+          diagnosticMode = "openFilesOnly",
         },
       },
     },
@@ -70,13 +70,13 @@ lspconfig.pyright.setup {
   commands = {
     PyrightOrganizeImports = {
       organize_imports,
-      description = 'Organize Imports',
+      description = "Organize Imports",
     },
     PyrightSetPythonPath = {
       set_python_path,
-      description = 'Reconfigure pyright with the provided python path',
+      description = "Reconfigure pyright with the provided python path",
       nargs = 1,
-      complete = 'file',
+      complete = "file",
     },
   },
   docs = {
