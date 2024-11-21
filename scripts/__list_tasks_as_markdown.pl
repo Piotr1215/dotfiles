465e3d3dd8e8ca1bbc17d0706dfc9fa23132088a
Piotr <decoder@live.de>
2024-11-21 10:28:18 +0100

    scripts: ability to list tasks with multiple labels
    Signed-off-by: Piotr <decoder@live.de>

--- scripts/__list_tasks_as_markdown.pl
+++ scripts/__list_tasks_as_markdown.pl
@@ -4,18 +4,17 @@ use warnings;
 use XML::LibXML;
 use Time::Piece;
 
-my $mode = shift @ARGV || '';
-my $last_week = ($mode eq '') ? 1 : 0;  # Add this flag
+# Convert command line arguments into an array of modes
+my @modes = map { s/^\+//; $_ } @ARGV;
+my $last_week = (@modes == 0) ? 1 : 0;  # Set last_week flag if no modes specified
 
 my $t = localtime;
 my $wday = $t->wdayname;
-
 my $days_to_subtract = ($wday eq 'Mon') ? 3 : 1;
-
 my $project_mappings_file = '/home/decoder/dev/dotfiles/scripts/__project_mappings.conf';
 my $xml_script = '/home/decoder/dev/dotfiles/scripts/__format_tasks_xml.pl';
-
 my %project_mappings;
+
 open my $map_fh, '<', $project_mappings_file or die "Could not open file '$project_mappings_file': $!";
 while (<$map_fh>) {
   if (/^\s*\[\s*"([^"]+)"\s*\]\s*=\s*"([^"]+)"/) {
@@ -31,7 +30,6 @@ $xml_content =~ s{(<description>.*?)(<)(.*?</description>)}{$1&lt;$3}g;
 
 my $parser = XML::LibXML->new;
 my $doc = $parser->load_xml(string => $xml_content);
-
 my %tasks_by_project;
 
 my ($sec, $min, $hour, $mday, $mon, $year) = localtime(time - $days_to_subtract * 24 * 60 * 60);
@@ -46,13 +44,28 @@ for my $task ($doc->findnodes('/tasks/task')) {
   my $end_date = $task->findvalue('end');
   $end_date =~ s/T.*//;
   my $tags = join ' ', $task->findnodes('tags/tag/text()');
-
+  
   next unless $tags =~ /\bwork\b/;
   next if $tags =~ /\bidea\b/;
-
-  next unless ($mode eq '+next' && $status eq 'pending' && $tags =~ /\bnext\b/) ||
-              ($mode eq '+pending' && $status eq 'pending') ||
-              ($mode eq '' && $status eq 'completed' && ($end_date eq $yesterday || ($last_week && $end_date ge $last_week_date)));
+  
+  # New task filtering logic for multiple modes
+  my $include_task = 0;
+  if (@modes) {
+    # Check if any of the specified modes match the tags
+    for my $mode (@modes) {
+      if ($mode eq 'pending') {
+        $include_task = 1 if $status eq 'pending';
+      } else {
+        $include_task = 1 if $tags =~ /\b$mode\b/ && $status eq 'pending';
+      }
+    }
+  } else {
+    # Original completed tasks logic
+    $include_task = 1 if $status eq 'completed' && 
+                        ($end_date eq $yesterday || ($last_week && $end_date ge $last_week_date));
+  }
+  
+  next unless $include_task;
 
   my $project_key = $task->findvalue('project') || ' ';
   my $project = $project_mappings{$project_key} || 'Unknown';
@@ -60,19 +73,18 @@ for my $task ($doc->findnodes('/tasks/task')) {
   my $description = $task->findvalue('description');
   my $anno_text = "";
   my $checkbox = ($status eq 'pending') ? "[ ]" : "[x]";
-
+  
   for my $anno ($task->findnodes('annotations/annotation')) {
     if ($anno->findvalue('description') =~ /(https:\/\/\S+)/) {
       $anno_text .= " [[${url_index}]]($1)";
       $url_index++;
     }
   }
-
+  
   push @{$tasks_by_project{$project}}, "$checkbox $description$anno_text";
 }
 
 print "----------------\n" if $last_week;
-
 for my $project (sort keys %tasks_by_project) {
   print "$project\n";
   for my $task (@{$tasks_by_project{$project}}) {
