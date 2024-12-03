6dcd2980dfcb64589e81516732171411164fe6db
Piotr <decoder@live.de>
2024-12-03 22:35:46 +0100

    nvim: mini.bracketed only with indent jumper
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/settings/plugins-setup.lua
+++ .config/nvim/lua/settings/plugins-setup.lua
@@ -248,6 +248,23 @@ require("mini.files").setup {
   },
 }
 
+require("mini.bracketed").setup {
+  buffer = { suffix = "", options = {} },
+  comment = { suffix = "", options = {} },
+  conflict = { suffix = "", options = {} },
+  diagnostic = { suffix = "", options = {} },
+  file = { suffix = "", options = {} },
+  indent = { suffix = "i", options = {} },
+  jump = { suffix = "", options = {} },
+  location = { suffix = "", options = {} },
+  oldfile = { suffix = "", options = {} },
+  quickfix = { suffix = "", options = {} },
+  treesitter = { suffix = "", options = {} },
+  undo = { suffix = "", options = {} },
+  window = { suffix = "", options = {} },
+  yank = { suffix = "", options = {} },
+}
+
 require("go").setup {
   gofmt = "gofumpt",
   lsp_gofumpt = true,
