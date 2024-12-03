c97c2d2fc1c6e2aabf96d87d69120b77f2f24b4a
Piotr <decoder@live.de>
2024-12-03 22:36:16 +0100

    nvim: limit flash.nvim to only s, it will probaby go too
    Signed-off-by: Piotr <decoder@live.de>

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -46,7 +46,6 @@ return require("lazy").setup {
         desc = "Resume the last yazi session",
       },
     },
-    ---@type YaziConfig
     opts = {
       -- if you want to open yazi instead of netrw, see below for more info
       open_for_directories = false,
@@ -73,8 +72,6 @@ return require("lazy").setup {
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
   },
   "MunifTanjim/nui.nvim",
