2c7d03060eaf3534d35bd0f2bbb4e5106bbbe353
Piotr Zaniewski <piotrzan@gmail.com>
2025-02-03 12:12:05 +0100

    scripts: upgrade nvim url changed

--- scripts/__upgrade-nvim.sh
+++ scripts/__upgrade-nvim.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 # curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
-sudo chmod u+x nvim.appimage
-sudo mv nvim.appimage /usr/local/bin/nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
+sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
+
