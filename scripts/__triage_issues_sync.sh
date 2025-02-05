0e3314d7e3ba1ebe3fd2162a39139012a21a6f77
Piotr Zaniewski <piotrzan@gmail.com>
2025-02-05 23:21:54 +0100

    task: remove issues if out of triage

--- scripts/__triage_issues_sync.sh
+++ scripts/__triage_issues_sync.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 set -eo pipefail
 source /home/decoder/dev/dotfiles/scripts/__lib_taskwarrior_interop.sh
-
 get_task_id_by_description() {
 	local description="$1"
 	task +triage status:pending export |
 		jq -r --arg desc "$description" '.[] | select(.description == $desc) | .uuid'
 }
-
 get_all_triage_tasks() {
 	task +triage status:pending export |
 		jq -r '.[] | .description'
 }
-
 get_triage_issues() {
 	curl -s -X POST \
 		-H "Content-Type: application/json" \
@@ -20,22 +17,18 @@ get_triage_issues() {
 		--data '{"query": "query { issues(filter: {state: {name: {eq: \"Triage\"}}, team: {name: {eq: \"Docs\"}}}) { nodes { id identifier number title url team { name } } } }"}' \
 		https://api.linear.app/graphql | jq -r '.data.issues.nodes[]'
 }
-
 main() {
 	local issue_title issue_id issue_url task_uuid
 	local issues_added=0
 	local linear_issues
-
 	linear_issues=$(get_triage_issues | jq -r '.title')
-
 	while read -r task_desc; do
 		if ! echo "$linear_issues" | grep -Fxq "$task_desc"; then
 			task_uuid=$(get_task_id_by_description "$task_desc")
-			echo "Removing triage tag from: $task_desc"
-			task "$task_uuid" modify -triage
+			echo "Deleting task no longer in triage: $task_desc"
+			echo "yes" | task "$task_uuid" delete
 		fi
 	done < <(get_all_triage_tasks)
-
 	while read -r issue; do
 		issue_title=$(echo "$issue" | jq -r '.title')
 		issue_id=$(echo "$issue" | jq -r '.identifier')
@@ -51,5 +44,4 @@ main() {
 		fi
 	done < <(get_triage_issues | jq -c '.')
 }
-
 main
