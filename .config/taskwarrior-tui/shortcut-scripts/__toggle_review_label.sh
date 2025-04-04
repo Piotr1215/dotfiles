bf59f65585dad4a4e41bb4970bcac887500307a9
Piotr Zaniewski <piotrzan@gmail.com>
2025-04-04 09:23:56 +0200

    scripts: toggle backlog instead of review

--- .config/taskwarrior-tui/shortcut-scripts/__toggle_review_label.sh
+++ .config/taskwarrior-tui/shortcut-scripts/__toggle_review_label.sh
@@ -4,10 +4,10 @@ uuid="$1"
 # Use task _tags command to get current tags
 current_tags=$(task _tags "$uuid")
 
-if echo "$current_tags" | grep -q "review"; then
-	# Remove review tag if present
-	task rc.bulk=0 rc.confirmation=off "$uuid" modify -review
+if echo "$current_tags" | grep -q "backlog"; then
+	# Remove backlog tag if present
+	task rc.bulk=0 rc.confirmation=off "$uuid" modify -backlog
 else
-	# Add review tag if not present
-	task rc.bulk=0 rc.confirmation=off "$uuid" modify +review
+	# Add backlog tag if not present
+	task rc.bulk=0 rc.confirmation=off "$uuid" modify +backlog
 fi
