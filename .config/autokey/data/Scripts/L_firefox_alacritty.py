540765f86bbe1d5fdea0239cecd22a3c37db799a
Piotr Zaniewski <piotrzan@gmail.com>
2025-05-17 15:56:34 +0200

    scripts: focus on alacritty in firefox|alacritty split

--- .config/autokey/data/Scripts/L_firefox_alacritty.py
+++ .config/autokey/data/Scripts/L_firefox_alacritty.py
@@ -6,3 +6,7 @@ layout = '/home/decoder/dev/dotfiles/scripts/__layouts.sh'
 
 subprocess.run([layout, "2"])
 
+# This focuses on alacritty once layout is active
+subprocess.run(["xdotool", "key", "super+shift+l"])
+
+
