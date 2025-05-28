915dd4d5108218ea87126ce447b11eb2f60855b2
Piotr Zaniewski <piotrzan@gmail.com>
2025-05-28 15:55:52 +0200

    taskwarrior: toggle kill label, backlog is not often used

--- .config/taskwarrior-tui/shortcut-scripts/__toggle_review_label.sh
+++ .config/taskwarrior-tui/shortcut-scripts/__toggle_review_label.sh
@@ -4,10 +4,10 @@ uuid="$1"
 # Use task _tags command to get current tags
 current_tags=$(task _tags "$uuid")
 
-if echo "$current_tags" | grep -q "backlog"; then
-	# Remove backlog tag if present
-	task rc.bulk=0 rc.confirmation=off "$uuid" modify -backlog
+if echo "$current_tags" | grep -q "kill"; then
+	# Remove kill tag if present
+	task rc.bulk=0 rc.confirmation=off "$uuid" modify -kill
 else
-	# Add backlog tag if not present
-	task rc.bulk=0 rc.confirmation=off "$uuid" modify +backlog
+	# Add kill tag if not present
+	task rc.bulk=0 rc.confirmation=off "$uuid" modify +kill
 fi
