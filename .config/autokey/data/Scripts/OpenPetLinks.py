fcbcade09503a18d3067a6ded99e166c042d55c2
Piotr Zaniewski <piotrzan@gmail.com>
2025-08-25 21:46:26 +0200

    scripts: bookmarks runner should be system-wide

--- .config/autokey/data/Scripts/OpenPetLinks.py
+++ .config/autokey/data/Scripts/OpenPetLinks.py
@@ -1,3 +1,2 @@
 import subprocess
-# Use tmux to run the script in a popup, just like the tmux binding does
-subprocess.run(["tmux", "display-popup", "-E", "/home/decoder/dev/dotfiles/scripts/__link_pane_runner.sh"])
+subprocess.run(["/home/decoder/dev/dotfiles/scripts/__link_pane_runner.sh"])
