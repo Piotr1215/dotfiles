93883d8e8cde2dc72a949da109653492f039aa99
Piotr Zaniewski <piotrzan@gmail.com>
2025-10-10 09:45:12 +0200

    task: adjust panes respawner after task simplificaiton

--- .config/taskwarrior-tui/shortcut-scripts/__toggle_backlog_label.sh
+++ .config/taskwarrior-tui/shortcut-scripts/__toggle_backlog_label.sh
@@ -4,10 +4,10 @@ uuid="$1"
 # Use task _tags command to get current tags
 current_tags=$(task _tags "$uuid")
 
-if echo "$current_tags" | grep -q "backlog"; then
-	# Remove backlog tag if present
-	task rc.bulk=0 rc.confirmation=off "$uuid" modify -backlog
+if echo "$current_tags" | grep -q "wt"; then
+	# Remove wt tag if present
+	task rc.bulk=0 rc.confirmation=off "$uuid" modify -wt
 else
-	# Add backlog tag if not present
-	task rc.bulk=0 rc.confirmation=off "$uuid" modify +backlog
+	# Add wt tag if not present
+	task rc.bulk=0 rc.confirmation=off "$uuid" modify +wt
 fi
