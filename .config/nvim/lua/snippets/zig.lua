b1edb09cbc2d2e51e31b6b258462ce0918a48a5a
Piotr Zaniewski <piotrzan@gmail.com>
2025-12-05 10:04:49 +0100

    nvim: setup basic zig lsp

---
+++ .config/nvim/lua/snippets/zig.lua
@@ -0,0 +1,11 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local fmt = require("luasnip.extras.fmt").fmt
+
+return {
+  -- Add your snippets here
+}
