f1dba586305d030aed60084d2f06a85233be79ba
Piotr Zaniewski <piotrzan@gmail.com>
2025-12-17 15:13:09 +0100

    scripts: move to librewolf

--- scripts/__open_youtube.sh
+++ scripts/__open_youtube.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Launch Firefox with Home profile and YouTube
+# Launch LibreWolf with Home profile and YouTube
 nohup firefox -P "Home" "https://youtube.com" > /dev/null 2>&1 &
-firefox_pid=$!
-xdotool search --pid $firefox_pid --onlyvisible --class Firefox windowactivate
+sleep 1
+xdotool search --classname Navigator --onlyvisible windowactivate
