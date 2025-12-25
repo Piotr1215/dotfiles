854d9ef5b0f32594c699bfac46a425906723064e
Piotr Zaniewski <piotrzan@gmail.com>
2025-12-25 10:15:06 +0100

    scripts: copy funciton

--- scripts/__copy_path_with_notification.sh
+++ scripts/__copy_path_with_notification.sh
@@ -10,7 +10,7 @@ NOTIFICATION_TIMEOUT="${COPY_NOTIFICATION_TIMEOUT:-1000}"
 path=$(~/dev/dotfiles/scripts/__extract_path_from_fzf.sh "$1")
 
 # Copy to clipboard without newline
-printf '%s' "$path" | xclip -selection clipboard
+~/dev/dotfiles/scripts/__clipboard_copy.sh "$path"
 
 # Show notification with the actual path
 notify-send -t "$NOTIFICATION_TIMEOUT" 'Path Copied' "$path"
